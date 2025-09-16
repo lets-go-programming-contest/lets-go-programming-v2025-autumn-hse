1a665b9fd11ec801367f5f4820a84c468318222a
JingolBong <oris.martyr@gmail.com>
2025-09-16 12:54:04 +0300

    [TASK-1] Test

---
+++ boris.martynov/task-1/internal/calculator/calculator.go
@@ -0,0 +1,61 @@
+package calc
+
+import (
+ "fmt"
+ "os"
+)
+
+func Add(a, b int64) int64 {
+ return a + b
+}
+
+func Subt(a, b int64) int64 {
+ return a - b
+}
+
+func Mul(a, b int64) int64 {
+ return a * b
+}
+
+func Div(a, b int64) float64 {
+ if b == 0 {
+  fmt.Println("Division by zero")
+  os.Exit(1)
+ }
+ return float64(a) / float64(b)
+}
+
+func UserInput() (int64, int64, string) {
+ var a, b int64
+ var operator string
+ var n int
+ var err error
+
+ fmt.Print("First number: ")
+ n, err = fmt.Scanln(&a)
+ if err != nil || n != 1 {
+  fmt.Println("Invalid first operand")
+  os.Exit(1)
+ }
+
+ fmt.Print("Second number: ")
+ n, err = fmt.Scanln(&b)
+ if err != nil || n != 1 {
+  fmt.Println("Invalid second operand")
+  os.Exit(1)
+ }
+
+ fmt.Print("Operation: ")
+ n, err = fmt.Scanln(&operator)
+ if err != nil || n != 1 {
+  fmt.Println("Invalid operation")
+  os.Exit(1)
+ }
+
+ if operator != "+" && operator != "-" && operator != "*" && operator != "/" {
+  fmt.Println("Invalid operation")
+  os.Exit(1)
+ }
+
+ return a, b, operator
+}

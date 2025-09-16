f4cdf3b734f7384b7eae770fcd11e727782c9f9b
JingolBong <oris.martyr@gmail.com>
2025-09-16 13:28:33 +0300

    [TASK-1] Fix5

--- boris.martynov/task-1/internal/calculator/calculator.go
+++ boris.martynov/task-1/internal/calculator/calculator.go
@@ -19,7 +19,7 @@ func Mul(a, b int64) int64 {
 func Div(a, b int64) float64 {
  if b == 0 {
   fmt.Println("Division by zero")
-  return 0, 0, ''
+  return 0, 0, ""
  }
  return float64(a) / float64(b)
 }
@@ -34,26 +34,26 @@ func UserInput() (int64, int64, string) {
  n, err = fmt.Scanln(&a)
  if err != nil || n != 1 {
   fmt.Println("Invalid first operand")
-  return 0, 0, ''
+  return 0, 0, ""
  }
 
  fmt.Print("Second number: ")
  n, err = fmt.Scanln(&b)
  if err != nil || n != 1 {
   fmt.Println("Invalid second operand")
-  return 0, 0, ''
+  return 0, 0, ""
  }
 
  fmt.Print("Operation: ")
  n, err = fmt.Scanln(&operator)
  if err != nil || n != 1 {
   fmt.Println("Invalid operation")
-  return 0, 0, ''
+  return 0, 0, ""
  }
 
  if operator != "+" && operator != "-" && operator != "*" && operator != "/" {
   fmt.Println("Invalid operation")
-  return 0, 0, ''
+  return 0, 0, ""
  }
 
  return a, b, operator

d8f96f619e97eb2c40c31c7b6a0867ca474e57ce
Ekaterina-101 <mnm54637@gmail.com>
2025-09-16 17:40:42 +0300

    [TASK-1] Fix tab

--- ekaterina.kuznetsova/task-1/cmd/service/main.go
+++ ekaterina.kuznetsova/task-1/cmd/service/main.go
@@ -5,39 +5,39 @@ import (
 )
 
 func main() {
-	var firstNum, secondNum int
-	var operation string
-	_, err1 := fmt.Scanln(&firstNum)
-	if err1 != nil {
-		fmt.Println("Invalid first operand")
-		return
-	}
-	_, err2 := fmt.Scanln(&secondNum)
-	if err2 != nil {
-		fmt.Println("Invalid second operand")
-		return
-	}
-	_, errOp := fmt.Scanln(&operation)
-	if errOp != nil {
-		fmt.Println("Invalid operation")
-		return
-	}
+    var firstNum, secondNum int
+    var operation string
+    _, err1 := fmt.Scanln(&firstNum)
+    if err1 != nil {
+        fmt.Println("Invalid first operand")
+        return
+    }
+    _, err2 := fmt.Scanln(&secondNum)
+    if err2 != nil {
+        fmt.Println("Invalid second operand")
+        return
+    }
+    _, errOp := fmt.Scanln(&operation)
+    if errOp != nil {
+        fmt.Println("Invalid operation")
+        return
+    }
 
-	if operation == "/" && secondNum == 0 {
-		fmt.Println("Division by zero")
-		return
-	}
+    if operation == "/" && secondNum == 0 {
+        fmt.Println("Division by zero")
+        return
+    }
 
-	switch operation {
-	case "+":
-		fmt.Println(firstNum + secondNum)
-	case "-":
-		fmt.Println(firstNum - secondNum)
-	case "*":
-		fmt.Println(firstNum * secondNum)
-	case "/":
-		fmt.Println(float64(firstNum) / float64(secondNum))
-	default:
-		fmt.Println("Invalid operation")
-	}
+    switch operation {
+    case "+":
+        fmt.Println(firstNum + secondNum)
+    case "-":
+        fmt.Println(firstNum - secondNum)
+    case "*":
+        fmt.Println(firstNum * secondNum)
+    case "/":
+        fmt.Println(float64(firstNum) / float64(secondNum))
+    default:
+        fmt.Println("Invalid operation")
+    }
 }

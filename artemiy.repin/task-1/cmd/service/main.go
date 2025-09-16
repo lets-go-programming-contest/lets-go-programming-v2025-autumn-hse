4914736a6b552f760cf48c7ade412d718a5a5131
Nevermind0911 <repinarti@gmail.com>
2025-09-16 23:56:29 +0300

    [TASK-1] First commit

---
+++ artemiy.repin/task-1/cmd/service/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var a, b float64
+	var op string
+
+	if _, err := fmt.Scanln(&a); err != nil {
+		fmt.Fprintln(os.Stderr, "invalid first operand")
+		return
+	}
+	if _, err := fmt.Scanln(&b); err != nil {
+		fmt.Fprintln(os.Stderr, "invalid second operand")
+		return
+	}
+	if _, err := fmt.Scanln(&op); err != nil {
+		fmt.Fprintln(os.Stderr, "invalid operation")
+		return
+	}
+
+	switch op {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Fprintln(os.Stderr, "division by zero")
+			return
+		}
+		fmt.Println(a / b)
+	default:
+		fmt.Fprintln(os.Stderr, "invalid operation")
+	}
+}

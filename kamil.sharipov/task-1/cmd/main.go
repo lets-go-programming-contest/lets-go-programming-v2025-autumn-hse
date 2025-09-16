41c244df0172ac008d27b12fffb849eb1b55f7a4
kamilSharipov <kamil5812@gmail.com>
2025-09-16 10:22:34 +0300

    [TASK-1] Initial commit

---
+++ kamil.sharipov/task-1/cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"Calculator/internal/calculator"
+	"fmt"
+)
+
+func main() {
+	var left_operand, right_operand int32
+	var operator string
+
+	_, err := fmt.Scanln(&left_operand)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scanln(&right_operand)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scanln(&operator)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	if operator != "+" && operator != "-" && operator != "*" && operator != "/" {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	result := calculator.Calculate(left_operand, right_operand, rune(operator[0]))
+	fmt.Printf("%d\n", result)
+}

94033329ef27ba97e13dadfcb7303f46ba62f2e2
German Feskov <g.feskov@yandex.ru>
2025-09-18 06:30:12 +0300

    [TASK-1] Rm: unused var

--- german.feskov/task-1/internal/calculator/calculator.go
+++ german.feskov/task-1/internal/calculator/calculator.go
@@ -9,24 +9,19 @@ type Operand int
 type Operation string
 
 func Calculate(a, b Operand, op Operation) (Operand, error) {
-	var (
-		result Operand
-		err    error
-	)
 	switch op {
 	case "+":
-		result, err = a+b, nil
+		return a + b, nil
 	case "-":
-		result, err = a-b, nil
+		return a - b, nil
 	case "*":
-		result, err = a*b, nil
+		return a * b, nil
 	case "/":
 		if b == 0 {
 			return 0, fmt.Errorf("Division by zero")
 		}
-		result, err = a/b, nil
+		return a / b, nil
 	default:
 		return 0, fmt.Errorf("Invalid operation")
 	}
-	return result, err
 }

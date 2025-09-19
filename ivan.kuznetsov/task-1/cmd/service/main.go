08165d879b087ada066699bc5438c6e61967d35f
Vanya <kuzid.24@gmail.com>
2025-09-19 10:29:50 +0300

    [TASK-1] Fixed

--- ivan.kuznetsov/task-1/cmd/service/main.go
+++ ivan.kuznetsov/task-1/cmd/service/main.go
@@ -3,35 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	var num1, num2 int
-	var operation string
+	var (
+		num1      int
+		num2      int
+		operation string
+	)
 
-	_, err1 := fmt.Scan(&num1)
-	_, err2 := fmt.Scan(&num2)
-	_, err3 := fmt.Scan(&operation)
-
-	if err1 != nil {
+	_, err := fmt.Scan(&num1)
+	if err != nil {
 		fmt.Println("Invalid first operand")
-	} else if err2 != nil {
+		return
+	}
+
+	_, err = fmt.Scan(&num2)
+	if err != nil {
 		fmt.Println("Invalid second operand")
-	} else if err3 != nil {
+		return
+	}
+
+	_, err = fmt.Scan(&operation)
+	if err != nil {
 		fmt.Println("Invalid operation")
-	} else {
-		switch operation {
-		case "+":
-			fmt.Println(num1 + num2)
-		case "-":
-			fmt.Println(num1 - num2)
-		case "*":
-			fmt.Println(num1 * num2)
-		case "/":
-			if num2 == 0 {
-				fmt.Println("Division by zero")
-			} else {
-				fmt.Println(num1 / num2)
-			}
-		default:
-			fmt.Println("Invalid operation")
+		return
+	}
+
+	switch operation {
+	case "+":
+		fmt.Println(num1 + num2)
+	case "-":
+		fmt.Println(num1 - num2)
+	case "*":
+		fmt.Println(num1 * num2)
+	case "/":
+		if num2 == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(num1 / num2)
 		}
+	default:
+		fmt.Println("Invalid operation")
 	}
 }

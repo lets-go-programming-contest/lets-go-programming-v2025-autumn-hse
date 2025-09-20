f38dd9056a64c084a9b26508ba68e6ad16fd8617
German Feskov <g.feskov@yandex.ru>
2025-09-20 23:07:51 +0300

    [TASK-2-2] Add: heapMax module, Fix: some visual, Add: error handling

--- german.feskov/task-2-2/cmd/service/main.go
+++ german.feskov/task-2-2/cmd/service/main.go
@@ -3,48 +3,30 @@ package main
 import (
 	"container/heap"
 	"fmt"
+
+	"github.com/6ermvH/task-2-2/internal/heapMax"
 )
-
-type IntHeap []int
-
-func (h *IntHeap) Len() int           { return len(*h) }
-func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
-func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
-
-func (h *IntHeap) Push(x any) {
-	v, ok := x.(int)
-	if !ok {
-		return
-	}
-
-	*h = append(*h, v)
-}
-
-func (h *IntHeap) Pop() any {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-
-	return x
-}
 
 func main() {
 	var (
 		dishCount int
-		dishes    = &IntHeap{}
 		kInd      int
 	)
 
+	dishes := &heapMax.IntHeap{}
 	heap.Init(dishes)
 
 	if _, err := fmt.Scan(&dishCount); err != nil {
+		fmt.Println(err)
+
 		return
 	}
 
 	for range dishCount {
 		var val int
 		if _, err := fmt.Scan(&val); err != nil {
+			fmt.Println(err)
+
 			return
 		}
 
@@ -52,6 +34,8 @@ func main() {
 	}
 
 	if _, err := fmt.Scan(&kInd); err != nil {
+		fmt.Println(err)
+
 		return
 	}
 

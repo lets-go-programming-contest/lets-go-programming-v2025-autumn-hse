80249fefdc9cddc8d0ae864b266d85f6adcd7cd6
Ekaterina-101 <mnm54637@gmail.com>
2025-09-20 21:11:40 +0300

    [TASK-1] Fix style

--- ekaterina.kuznetsova/task-1/cmd/service/main.go
+++ ekaterina.kuznetsova/task-1/cmd/service/main.go
@@ -6,25 +6,23 @@ import (
 
 func main() {
 	var firstNum, secondNum int
-	var operation string
+	operation string
+
 	_, err1 := fmt.Scanln(&firstNum)
 	if err1 != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
+
 	_, err2 := fmt.Scanln(&secondNum)
 	if err2 != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
+
 	_, errOp := fmt.Scanln(&operation)
 	if errOp != nil {
 		fmt.Println("Invalid operation")
-		return
-	}
-
-	if operation == "/" && secondNum == 0 {
-		fmt.Println("Division by zero")
 		return
 	}
 
@@ -36,6 +34,10 @@ func main() {
 	case "*":
 		fmt.Println(firstNum * secondNum)
 	case "/":
+		if secondNum == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
 		fmt.Println(float64(firstNum) / float64(secondNum))
 	default:
 		fmt.Println("Invalid operation")

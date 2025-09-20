3e0762ad2263f13a32fcd1021a1ef0757a18c60d
German Feskov <g.feskov@yandex.ru>
2025-09-20 23:07:51 +0300

    [TASK-2-2] Add: heapMax module, Fix: some visual, Add: error handling

---
+++ german.feskov/task-2-2/internal/heapMax/heapMax.go
@@ -0,0 +1,25 @@
+package heapMax
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int           { return len(*h) } // lint warn if has (*IntHeap) and (IntHeap) methods
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *IntHeap) Push(x any) {
+	v, ok := x.(int)
+	if !ok {
+		return
+	}
+
+	*h = append(*h, v)
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+
+	return x
+}

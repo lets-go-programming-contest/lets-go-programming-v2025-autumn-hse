3295bc4bf965e04c97e7a4a0ed10bfcacd47fb3d
Vanya <kuzid.24@gmail.com>
2025-09-21 09:21:57 +0300

    [TASK-1] Fixed2

--- ivan.kuznetsov/task-1/cmd/service/main.go
+++ ivan.kuznetsov/task-1/cmd/service/main.go
@@ -4,9 +4,8 @@ import "fmt"
 
 func main() {
 	var (
-		num1      int
-		num2      int
-		operation string
+		num1, num2 int
+		operation  string
 	)
 
 	_, err := fmt.Scan(&num1)
@@ -37,9 +36,9 @@ func main() {
 	case "/":
 		if num2 == 0 {
 			fmt.Println("Division by zero")
-		} else {
-			fmt.Println(num1 / num2)
+			return
 		}
+		fmt.Println(num1 / num2)
 	default:
 		fmt.Println("Invalid operation")
 	}

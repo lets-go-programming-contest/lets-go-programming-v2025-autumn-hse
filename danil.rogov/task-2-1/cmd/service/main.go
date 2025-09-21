f75da36736dd9b0d1c7041d52bb3ce2db5c61c2d
User <dvrogov06@yandex.ru>
2025-09-21 12:05:33 +0300

    [TASK-2-1] adding package

--- danil.rogov/task-2-1/cmd/service/main.go
+++ danil.rogov/task-2-1/cmd/service/main.go
@@ -2,85 +2,21 @@ package main
 
 import (
 	"fmt"
-)
 
-func maxInt(a, b int) int {
-	if a > b {
-		return a
-	}
-
-	return b
-}
-
-func minInt(a, b int) int {
-	if a > b {
-		return b
-	}
-
-	return a
-}
-
-const (
-	minimumTemperature = 15
-	maximumTemperature = 30
-	errorTemperature   = -1
+	"github.com/Tapochek2894/task-2/subtask-1/internal/temperature"
 )
 
 func main() {
-	var departmentCount, employeeCount int
+	var departmentCount int
 
 	_, err := fmt.Scan(&departmentCount)
 	if err != nil {
-		fmt.Println("Error reading input:", err)
+		fmt.Println("Error reading department count:", err)
 
 		return
 	}
 
 	for range departmentCount {
-		_, err = fmt.Scan(&employeeCount)
-		if err != nil {
-			fmt.Println("Error reading input:", err)
-
-			return
-		}
-
-		lowerBound := minimumTemperature
-		upperBound := maximumTemperature
-
-		for range employeeCount {
-			var (
-				preferredTemperature int
-				inequalitySign       string
-			)
-
-			_, err = fmt.Scan(&inequalitySign, &preferredTemperature)
-			if err != nil {
-				fmt.Println("Error reading input:", err)
-
-				return
-			}
-
-			if preferredTemperature < minimumTemperature || preferredTemperature > maximumTemperature {
-				fmt.Println(errorTemperature)
-
-				continue
-			}
-
-			switch inequalitySign {
-			case ">=":
-				lowerBound = maxInt(lowerBound, preferredTemperature)
-			case "<=":
-				upperBound = minInt(upperBound, preferredTemperature)
-			}
-
-			result := lowerBound
-			if lowerBound > upperBound {
-				result = errorTemperature
-			}
-
-			fmt.Println(result)
-		}
-
-		fmt.Println()
+		temperature.SetDepartmentTemperature()
 	}
 }
---
+++ danil.rogov/task-2-1/internal/temperature/temperature.go
@@ -0,0 +1,60 @@
+package temperature
+
+import (
+	"fmt"
+
+	"github.com/Tapochek2894/task-2/subtask-1/internal/intmath"
+)
+
+const (
+	minimumTemperature = 15
+	maximumTemperature = 30
+	errorTemperature   = -1
+)
+
+func SetDepartmentTemperature() {
+	var employeeCount int
+	_, err := fmt.Scan(&employeeCount)
+	if err != nil {
+		fmt.Println("Error reading employee count:", err)
+
+		return
+	}
+
+	lowerBound := minimumTemperature
+	upperBound := maximumTemperature
+
+	for range employeeCount {
+		var (
+			preferredTemperature int
+			inequalitySign       string
+		)
+
+		_, err = fmt.Scan(&inequalitySign, &preferredTemperature)
+		if err != nil {
+			fmt.Println("Error reading sign or temperature:", err)
+
+			return
+		}
+
+		if preferredTemperature < minimumTemperature || preferredTemperature > maximumTemperature {
+			fmt.Println(errorTemperature)
+
+			continue
+		}
+
+		switch inequalitySign {
+		case ">=":
+			lowerBound = intmath.LargerInt(lowerBound, preferredTemperature)
+		case "<=":
+			upperBound = intmath.SmallerInt(upperBound, preferredTemperature)
+		}
+
+		result := lowerBound
+		if lowerBound > upperBound {
+			result = errorTemperature
+		}
+
+		fmt.Println(result)
+	}
+}

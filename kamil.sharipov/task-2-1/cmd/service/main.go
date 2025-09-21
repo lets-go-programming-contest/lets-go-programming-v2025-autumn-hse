a25a768b05625390081db23fe0680809b0aaec8b
kamilSharipov <kamil5812@gmail.com>
2025-09-21 19:35:00 +0300

    [TASK-2-1] Initial

---
+++ kamil.sharipov/task-2-1/cmd/service/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+const (
+	minStartingTemp = 15
+	maxStartingTemp = 30
+)
+
+func main() {
+	var (
+		numOfDepartments, employees, temp      int
+		minComfortableTemp, maxComfortableTemp int
+		operator                               string
+	)
+
+	_, err := fmt.Scanln(&numOfDepartments)
+	if err != nil {
+		return
+	}
+
+	for i := 0; i < numOfDepartments; i++ {
+		_, err := fmt.Scanln(&employees)
+		if err != nil {
+			return
+		}
+
+		minComfortableTemp = minStartingTemp
+		maxComfortableTemp = maxStartingTemp
+		for j := 0; j < employees; j++ {
+			_, err = fmt.Scanln(&operator, &temp)
+			if err != nil {
+				return
+			}
+			switch operator {
+			case "<=":
+				maxComfortableTemp = min(maxComfortableTemp, temp)
+			case ">=":
+				minComfortableTemp = max(minComfortableTemp, temp)
+			}
+			if minComfortableTemp > maxComfortableTemp {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(minComfortableTemp)
+			}
+		}
+	}
+}

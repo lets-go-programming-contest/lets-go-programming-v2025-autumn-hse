e5b8e609368771a47a36619b186b52825747523f
kamilSharipov <kamil5812@gmail.com>
2025-09-22 23:10:01 +0300

    [TASK-2-2] Release

--- kamil.sharipov/task-2-2/cmd/service/main.go
+++ kamil.sharipov/task-2-2/cmd/service/main.go
@@ -5,24 +5,50 @@ import (
 	"fmt"
 )
 
+type IntHeap []int
+
+func (heap IntHeap) Len() int {
+	return len(heap)
+}
+
+func (heap IntHeap) Less(i, j int) bool {
+	return heap[i] > heap[j]
+}
+
+func (heap IntHeap) Swap(i, j int) {
+	heap[i], heap[j] = heap[j], heap[i]
+}
+
+func (heap *IntHeap) Push(x interface{}) {
+	*heap = append(*heap, x.(int))
+}
+
+func (heap *IntHeap) Pop() interface{} {
+	oldHeap := *heap
+	x := oldHeap[len(oldHeap)-1]
+	*heap = oldHeap[0 : len(oldHeap)-1]
+	return x
+}
+
 func main() {
 	var (
-		dishCount, dish, rank int
+		dishCount, dish, rank, ans int
 	)
 
-	_, err = fmt.Scanln(&dishCount)
+	_, err := fmt.Scanln(&dishCount)
 	if err != nil {
 		return
 	}
 
-	type IntHeap []int
+	h := &IntHeap{}
+	heap.Init(h)
 
 	for range dishCount {
-		_, err = fmt.Scan(&dish)
+		_, err := fmt.Scan(&dish)
 		if err != nil {
 			return
 		}
-
+		heap.Push(h, dish)
 	}
 
 	_, err = fmt.Scanln(&rank)
@@ -30,4 +56,12 @@ func main() {
 		return
 	}
 
+	for range rank {
+		ans = heap.Pop(h).(int)
+	}
+
+	_, err = fmt.Println(ans)
+	if err != nil {
+		return
+	}
 }

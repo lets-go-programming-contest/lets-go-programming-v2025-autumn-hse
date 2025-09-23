ee0113690a3e56636710273e1e74162214bba931
Vladislav <mainmail30@bk.ru>
2025-09-23 13:55:23 -0400

    [TASK-1] Initial commit

--- vladislav.peleev/task-1/cmd/service/main.go
+++ vladislav.peleev/task-1/cmd/service/main.go
@@ -1,57 +1,64 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
+	"fmt"
+	"strconv"
 )
 
-func read_number() (int, error) {
-    var input string
-    fmt.Scan(&input)
-    num, err := strconv.Atoi(input)
-    if err != nil {
-        return 0, fmt.Errorf("")
-    }
-    return num, nil
+func readNumber() (int, error) {
+	var input string
+	_, err := fmt.Scan(&input)
+	if err != nil {
+		return 0, fmt.Errorf("readNumber() : Scan error")
+	}
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		return 0, fmt.Errorf("readNumber() : Conversion error")
+	}
+	return num, nil
 }
 
 func main() {
-    var a, b int
-    var op string
-   
-    a, err := read_number()
-    if err != nil {
-        fmt.Println("Invalid first operand")
-        return
-    }
-    
-    b, err = read_number()
-    if err != nil {
-        fmt.Println("Invalid second operand")
-	return
-    }
+	var a, b int
+	var op string
 
-    fmt.Scan(&op)
+	a, err := readNumber()
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
 
-    if op == "+" {
-        s := a + b
-        fmt.Println(s)
-    } else if op == "-" {
-        s := a - b
-        fmt.Println(s)
-    } else if op == "*" {
-        s := a * b
-        fmt.Println(s)
-    } else if op == "/" {
-        if b == 0 {
-            fmt.Println("Division by zero")
-            return
-        } else {
-            s := a / b
-            fmt.Println(s)
-        }
-    } else {
-        fmt.Println("Invalid operation")
-        return
-    }
+	b, err = readNumber()
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scan(&op)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch op {
+	case "+":
+		s := a + b
+		fmt.Println(s)
+	case "-":
+		s := a - b
+		fmt.Println(s)
+	case "*":
+		s := a * b
+		fmt.Println(s)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		s := a / b
+		fmt.Println(s)
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
 }

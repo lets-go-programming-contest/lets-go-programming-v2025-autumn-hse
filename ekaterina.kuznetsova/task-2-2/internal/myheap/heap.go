bf19e693c1a39450e9f9fe03c1a1239bfe595709
Ekaterina-101 <mnm54637@gmail.com>
2025-09-23 17:45:35 +0300

    [TASK-2-2] Fix format

--- ekaterina.kuznetsova/task-2-2/internal/myheap/heap.go
+++ ekaterina.kuznetsova/task-2-2/internal/myheap/heap.go
@@ -2,8 +2,13 @@ package myheap
 
 type IntHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] > h[j]
+}
 func (h IntHeap) Swap(i, j int) {
 	tmp := h[i]
 	h[i] = h[j]

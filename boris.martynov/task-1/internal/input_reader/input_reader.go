ef26ad08c3c3a93a5e55ebd760fe8d27f90fea31
bamartynov <bamartynov@edu.hse.ru>
2025-09-26 14:26:05 +0300

    [TASK-1] mega_supper_last_fix

--- boris.martynov/task-1/internal/input_reader/input_reader.go
+++ boris.martynov/task-1/internal/input_reader/input_reader.go
@@ -6,26 +6,24 @@ import (
 	"fmt"
 )
 
-var (
-	ErrDivisionByZero   = errors.New("Division by zero")
-	ErrInvalidNumber    = errors.New("Invalid ")
-	ErrInvalidOperation = errors.New("Invalid operation")
-)
-
 func ReadNumber(name string) (int64, error) {
 	var n int64
-	read, err := fmt.Scanln(&n)
-	if err != nil || read != 1 {
-		return 0, fmt.Errorf("%w: %s", ErrInvalidNumber, name)
+	_, err := fmt.Scanln(&n)
+	if err != nil {
+		if name == "first operand" {
+			return 0, errors.New("Invalid first operand")
+		} else {
+			return 0, errors.New("Invalid second operand")
+		}
 	}
 	return n, nil
 }
 
 func ReadOperator() (string, error) {
 	var op string
-	read, err := fmt.Scanln(&op)
-	if err != nil || read != 1 {
-		return "", ErrInvalidOperation
+	_, err := fmt.Scanln(&op)
+	if err != nil {
+		return "", errors.New("Invalid operation")
 	}
 	return op, nil
 }

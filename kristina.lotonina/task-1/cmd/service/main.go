5695ebbaedb6b1d0acfe4317790fd09645e458a2
kristina <kristinalotonina@yandex.ru>
2025-09-26 15:54:53 +0300

    [TASK-1] fix error return

--- kristina.lotonina/task-1/cmd/service/main.go
+++ kristina.lotonina/task-1/cmd/service/main.go
@@ -12,14 +12,14 @@ func main() {
 		operation     string
 	)
 	_, err1 := fmt.Scan(&first)
-	_, err2 := fmt.Scan(&second)
-	_, err3 := fmt.Scan(&operation)
 	if err1 != nil {
 		fmt.Println("Invalid first operand")
 	}
+	_, err2 := fmt.Scan(&second)
 	if err2 != nil {
 		fmt.Println("Invalid second operand")
 	}
+	_, err3 := fmt.Scan(&operation)
 	if err3 != nil {
 		fmt.Println("Invalid operation")
 	}

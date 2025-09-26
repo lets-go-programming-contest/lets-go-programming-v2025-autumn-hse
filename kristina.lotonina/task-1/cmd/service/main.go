d5a402ad31c3c2a1e65dadb8b7ee6301992809fc
kristina <kristinalotonina@yandex.ru>
2025-09-26 15:11:35 +0300

    [TASK-1] fix if condition

--- kristina.lotonina/task-1/cmd/service/main.go
+++ kristina.lotonina/task-1/cmd/service/main.go
@@ -12,17 +12,17 @@ func main() {
 		operation     string
 	)
 	_, err := fmt.Scan(&first)
-	if (err != nil) {
+	if err != nil {
 		fmt.Print("Invalid first operand")
 		return
 	}
 	_, err = fmt.Scan(&second)
-	if (err != nil) {
+	if err != nil {
 		fmt.Print("Invalid second operand")
 		return
 	}
 	_, err = fmt.Scan(&operation)
-	if (err != nil) {
+	if err != nil {
 		fmt.Print("Invalid operation")
 		return
 	}
--- kristina.lotonina/task-1/internal/calculator/calculator.go
+++ kristina.lotonina/task-1/internal/calculator/calculator.go
@@ -3,14 +3,14 @@ package calculator
 import "fmt"
 
 func Calculate(a int, b int, op string) {
-	if (op == "+") {
+	if op == "+" {
 		fmt.Print(a + b) 
-	} else if (op == "-") {
+	} else if op == "-" {
 		fmt.Print(a - b)
-	} else if (op == "*") {
+	} else if op == "*" {
 		fmt.Print(a * b)
-	} else if (op == "/") {
-		if (b != 0) {
+	} else if op == "/" {
+		if b != 0 {
 			fmt.Print(a / b)
 		} else {
 			fmt.Print("Division by zero\n")	

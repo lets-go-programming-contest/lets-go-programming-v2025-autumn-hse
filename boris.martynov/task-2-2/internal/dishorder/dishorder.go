1c804c088314b67cb1f1a7c8ba0b4a696fc41b58
bamartynov <bamartynov@edu.hse.ru>
2025-09-27 16:22:00 +0300

    [TASK-2-2] fix_linters_2

--- boris.martynov/task-2-2/internal/dishorder/dishorder.go
+++ boris.martynov/task-2-2/internal/dishorder/dishorder.go
@@ -2,9 +2,9 @@ package dishorder
 
 type PrefOrder []int
 
-func (h PrefOrder) Len() int           { return len(h) }
-func (h PrefOrder) Less(i, j int) bool { return h[i] > h[j] }
-func (h PrefOrder) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h PrefOrder) Len() int           { return len(h) }           //nolint
+func (h PrefOrder) Less(i, j int) bool { return h[i] > h[j] }      //nolint
+func (h PrefOrder) Swap(i, j int)      { h[i], h[j] = h[j], h[i] } //nolint
 
 func (h *PrefOrder) Push(x any) {
 	if pushedInt, noerr := x.(int); noerr {
@@ -18,8 +18,10 @@ func (h *PrefOrder) Pop() any {
 	if len(*h) == 0 {
 		panic("Pop emty")
 	}
+
 	old := *h
 	x := old[len(old)-1]
+
 	*h = old[0 : len(old)-1]
 
 	return x

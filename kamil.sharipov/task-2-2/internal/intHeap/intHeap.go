afaaaac62b0c8c13f37d914575f0a3f1ed3cb241
kamilSharipov <kamil5812@gmail.com>
2025-09-27 11:51:27 +0300

    [TASK-2-2] changes

---
+++ kamil.sharipov/task-2-2/internal/intHeap/intHeap.go
@@ -0,0 +1,44 @@
+package intHeap
+
+import (
+	"container/heap"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] > h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	rating, ok := x.(int)
+	if !ok {
+		panic("IntHeap.Push: expected int")
+	}
+	*h = append(*h, rating)
+}
+
+func (h *IntHeap) Pop() interface{} {
+	if h.Len() == 0 {
+		panic("IntHeap.Pop: cannot pop from empty heap")
+	}
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func InitIntHeap() *IntHeap {
+	h := &IntHeap{}
+	heap.Init(h)
+	return h
+}

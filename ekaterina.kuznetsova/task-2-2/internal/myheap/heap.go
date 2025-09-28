e79c3ec234209892f5967f6917cd5e8a87d999e1
Ekaterina-101 <mnm54637@gmail.com>
2025-09-28 18:27:54 +0300

    [TASK-2-2] Fix style 2

--- ekaterina.kuznetsova/task-2-2/internal/myheap/heap.go
+++ ekaterina.kuznetsova/task-2-2/internal/myheap/heap.go
@@ -3,44 +3,43 @@ package myheap
 import "errors"
 
 var (
-    errCast      = errors.New("cast error")
-    errEmptyData = errors.New("data is empty")
+	errCast      = errors.New("cast error")
+	errEmptyData = errors.New("data is empty")
 )
 
+//nolint:recvcheck // is linked type
 type IntHeap []int
 
 func (h IntHeap) Len() int {
-    return len(h)
+	return len(h)
 }
 
 func (h IntHeap) Less(i, j int) bool {
-    return h[i] > h[j]
+	return h[i] > h[j]
 }
 
 func (h IntHeap) Swap(i, j int) {
-    tmp := h[i]
-	h[i] = h[j]
-	h[j] = tmp
+	h[i], h[j] = h[j], h[i]
 }
 
 func (h *IntHeap) Push(x any) {
-    value, ok := x.(int)
-    if !ok {
-        panic(errCast)
-    }
+	value, ok := x.(int)
+	if !ok {
+		panic(errCast)
+	}
 
-    *h = append(*h, value)
+	*h = append(*h, value)
 }
 
 func (h *IntHeap) Pop() any {
-    if len(*h) == 0 {
-        panic(errEmptyData)
-    }
+	if len(*h) == 0 {
+		panic(errEmptyData)
+	}
 
-    old := *h
-    n := len(old)
-    x := old[n-1]
-    *h = old[0 : n-1]
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
 
-    return x
+	return x
 }

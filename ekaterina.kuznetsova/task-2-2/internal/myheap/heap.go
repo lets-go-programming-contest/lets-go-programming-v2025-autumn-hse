15a22465043fc2e8f2177707abd3f8bcfa93234b
Ekaterina-101 <mnm54637@gmail.com>
2025-09-28 18:08:55 +0300

    [TASK-2-2] Fix logical

--- ekaterina.kuznetsova/task-2-2/internal/myheap/heap.go
+++ ekaterina.kuznetsova/task-2-2/internal/myheap/heap.go
@@ -1,5 +1,7 @@
 package myheap
 
+import "errors"
+
 type IntHeap []int
 
 func (h IntHeap) Len() int {
@@ -9,6 +11,7 @@ func (h IntHeap) Len() int {
 func (h IntHeap) Less(i, j int) bool {
 	return h[i] > h[j]
 }
+
 func (h IntHeap) Swap(i, j int) {
 	tmp := h[i]
 	h[i] = h[j]
@@ -16,13 +19,23 @@ func (h IntHeap) Swap(i, j int) {
 }
 
 func (h *IntHeap) Push(x any) {
-	*h = append(*h, x.(int))
+	value, ok := x.(int)
+	if !ok {
+		panic(errors.New("cast error"))
+	}
+
+	*h = append(*h, value)
 }
 
 func (h *IntHeap) Pop() any {
+	if len(old) == 0 {
+		panic(errors.New("data is empty"))
+	}
+
 	old := *h
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }

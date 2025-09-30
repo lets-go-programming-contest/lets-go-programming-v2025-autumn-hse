8ff9795bf9182fe5adabf7b0e5d6ab7ad87bb95a
Anfisa111 <asfilipova@gmail.com>
2025-09-30 16:37:07 -0400

    [TASK-1] (#6)
    * [TASK-1] Initial commit
    
    * [TASK-1] Read checks added
    
    * [TASK-1] New read check added
    
    * [TASK-1] Format correction
    
    * [TASK-1] code improvement
    
    * [TASK-1] code improvement2
    
    * [TASK-1] improvement3
    
    * [TASK-1] improvement4
    
    * [TASK-1] remove pkg directory
    
    * [TASK-1] changed float64 to int

---
+++ anfisa.filipova/task-1/cmd/service/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"calculator/mycalc"
+	"fmt"
+)
+
+func main() {
+	var (
+		num1, num2 int
+		operation  string
+	)
+	_, err := fmt.Scan(&num1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&num2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, _ = fmt.Scan(&operation)
+	result, err := mycalc.Calculate(num1, num2, operation)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(result)
+}

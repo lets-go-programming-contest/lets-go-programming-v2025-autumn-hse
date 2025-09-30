e198d09192a517d7e15518ceb28b59b0b79ed179
osboxes.org <asfilipova@gmail.com>
2025-09-30 09:41:21 -0400

    [TASK-1] changed float64 to int

--- anfisa.filipova/task-1/mycalc/operations.go
+++ anfisa.filipova/task-1/mycalc/operations.go
@@ -4,22 +4,24 @@ import (
 	"errors"
 )
 
-var errDivisionByZero = errors.New("Division by zero")
-var errInvalidOperation = errors.New("Invalid operation")
+var (
+	errDivisionByZero   = errors.New("Division by zero")
+	errInvalidOperation = errors.New("Invalid operation")
+)
 
-func Calculate(operand1 int, operand2 int, operation string) (float64, error) {
+func Calculate(operand1 int, operand2 int, operation string) (int, error) {
 	switch operation {
 	case "+":
-		return float64(operand1 + operand2), nil
+		return operand1 + operand2, nil
 	case "-":
-		return float64(operand1 - operand2), nil
+		return operand1 - operand2, nil
 	case "*":
-		return float64(operand1 * operand2), nil
+		return operand1 * operand2, nil
 	case "/":
 		if operand2 == 0 {
 			return 0, errDivisionByZero
 		}
-		return float64(operand1) / float64(operand2), nil
+		return operand1 / operand2, nil
 	default:
 		return 0, errInvalidOperation
 	}

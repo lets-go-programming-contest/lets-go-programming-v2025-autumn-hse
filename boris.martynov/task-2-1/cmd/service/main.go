8d3e570263cfe9ff44b165889a0e7388e849422e
bamartynov <bamartynov@edu.hse.ru>
2025-09-30 18:03:00 +0300

    [TASK-2-1] fixes_requested

--- boris.martynov/task-2-1/cmd/service/main.go
+++ boris.martynov/task-2-1/cmd/service/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/JingolBong/task-2-1/internal/conditioner"
 )
 
+var errFailedToScan = errors.New("invalid input")
+
 func main() {
 	var numberOfDepartments int
 	if _, err := fmt.Scanln(&numberOfDepartments); err != nil {
+		fmt.Println(errFailedToScan)
+
 		return
 	}
 
--- boris.martynov/task-2-1/internal/conditioner/conditioner.go
+++ boris.martynov/task-2-1/internal/conditioner/conditioner.go
@@ -1,47 +1,73 @@
 package conditioner
 
 import (
+	"errors"
 	"fmt"
 )
+
+var errFailedToScan = errors.New("invalid input")
 
 const (
 	MinTemp = 15
 	MaxTemp = 30
 )
 
-func TemperatureWantedDepartment() {
-	var (
-		departmentCapacity, temperatureWantedByEmployee int
-		greaterOrLess                                   string
-	)
+type temperature struct {
+	lowest  int
+	highest int
+}
 
-	lowestTemperature := MinTemp
-	highestTemperature := MaxTemp
+func newTemperature(min, max int) *temperature {
+	return &temperature{
+		lowest:  min,
+		highest: max,
+	}
+}
+
+func (tr *temperature) update(greaterOrLess string, temp int) {
+	switch greaterOrLess {
+	case ">=":
+		if temp > tr.lowest {
+			tr.lowest = temp
+		}
+	case "<=":
+		if temp < tr.highest {
+			tr.highest = temp
+		}
+	}
+}
+
+func (tr *temperature) getResult() int {
+	if tr.lowest <= tr.highest {
+		return tr.lowest
+	}
+	return -1
+}
+
+func TemperatureWantedDepartment() {
+	var departmentCapacity int
 
 	if _, err := fmt.Scanln(&departmentCapacity); err != nil {
+		fmt.Println(errFailedToScan)
+
 		return
 	}
 
+	tr := newTemperature(MinTemp, MaxTemp)
+
 	for range departmentCapacity {
+		var (
+			temperatureWantedByEmployee int
+			greaterOrLess               string
+		)
+
 		if _, err := fmt.Scanln(&greaterOrLess, &temperatureWantedByEmployee); err != nil {
+			fmt.Println(errFailedToScan)
+
 			return
 		}
 
-		switch greaterOrLess {
-		case ">=":
-			if temperatureWantedByEmployee >= lowestTemperature {
-				lowestTemperature = temperatureWantedByEmployee
-			}
-		case "<=":
-			if temperatureWantedByEmployee <= highestTemperature {
-				highestTemperature = temperatureWantedByEmployee
-			}
-		}
-
-		if lowestTemperature <= highestTemperature {
-			fmt.Println(lowestTemperature)
-		} else {
-			fmt.Println("-1")
-		}
+		tr.update(greaterOrLess, temperatureWantedByEmployee)
+		fmt.Println(tr.getResult())
 	}
 }

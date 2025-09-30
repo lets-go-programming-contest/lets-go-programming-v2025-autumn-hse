2c07bc396421d5714cca6c74d4350b82e2cc8503
Vanya <kuzid.24@gmail.com>
2025-10-01 01:14:57 +0300

    [TASK-2-1] Changes: temperature storage and calculation logic

--- ivan.kuznetsov/task-2-1/internal/temperature/temperature.go
+++ ivan.kuznetsov/task-2-1/internal/temperature/temperature.go
@@ -1,20 +1,38 @@
 package temperature
 
-func OptimalTemperature(sign string, value int, numbers []int) []int {
-	var result []int
+import "fmt"
 
-	for _, temp := range numbers {
-		switch sign {
-		case "<=":
-			if temp <= value {
-				result = append(result, temp)
-			}
-		case ">=":
-			if temp >= value {
-				result = append(result, temp)
-			}
-		}
+type TemperatureRange struct {
+	min int
+	max int
+}
+
+func InitialRange() *TemperatureRange {
+	return &TemperatureRange{
+		min: 15,
+		max: 30,
 	}
+}
 
-	return result
+func OptimalTemperature(sign string, value int, tr *TemperatureRange) *TemperatureRange {
+	switch sign {
+	case "<=":
+		if value < tr.max {
+			tr.max = value
+		}
+	case ">=":
+		if value > tr.min {
+			tr.min = value
+		}
+	default:
+		fmt.Printf("Invalid comparison sign '%s'\nThe temperature range has not changed\n", sign)
+	}
+	return tr
+}
+
+func GetOptimalTemperature(tr *TemperatureRange) int {
+	if tr.min > tr.max {
+		return -1
+	}
+	return tr.min
 }

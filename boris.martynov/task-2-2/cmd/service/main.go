1905ec84523a5a2c786bd377806d2d07dd8a9f30
bamartynov <bamartynov@edu.hse.ru>
2025-09-30 17:24:16 +0300

    [TASK-2-2] fixes

--- boris.martynov/task-2-2/cmd/service/main.go
+++ boris.martynov/task-2-2/cmd/service/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"slices"
 
 	"github.com/JingolBong/task-2-2/internal/dishorder"
 )
 
+var errFailedToScan = errors.New("Invalid input")
+
 func main() {
-	var numberOfDishes, preferedDishNumb int
+	var numberOfDishes int
 
 	containerOfDishes := &dishorder.PrefOrder{}
 
 	if _, err := fmt.Scan(&numberOfDishes); err != nil {
+		fmt.Println(errFailedToScan)
 		return
 	}
 
@@ -20,13 +24,16 @@ func main() {
 		var dishPrefNumber int
 
 		if _, err := fmt.Scan(&dishPrefNumber); err != nil {
+			fmt.Println(errFailedToScan)
 			return
 		}
 
 		containerOfDishes.Push(dishPrefNumber)
 	}
 
+	var preferedDishNumb int
 	if _, err := fmt.Scanln(&preferedDishNumb); err != nil {
+		fmt.Println(errFailedToScan)
 		return
 	}
 

92dfccba086170c7fc23b75f2c755b27f3c8baaf
OlesiaOl <olesia.olshv@gmail.com>
2025-10-02 00:15:20 +0300

    [TASK-2-2] package

---
+++ olesia.olshevskaia/task-2-2/intheap/intheap.go
@@ -0,0 +1,44 @@
+package intheap
+
+import (
+	"fmt"
+)
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int {
+	return len(*h)
+}
+
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j]
+}
+
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *IntHeap) Push(x any) {
+	newX, ok := x.(int)
+	if !ok {
+		panic("intHeap.Push: invalid type, expected int")
+	}
+
+	*h = append(*h, newX)
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	length := len(*h)
+
+	if length == 0 {
+		fmt.Println("intHeap is empty")
+
+		return nil
+	}
+
+	x := old[length-1]
+	*h = old[:length-1]
+
+	return x
+}

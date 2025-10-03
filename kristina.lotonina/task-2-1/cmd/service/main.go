b29e54eaefffaff9b8626481699447bdaedf2f47
kristina <kristinalotonina@yandex.ru>
2025-10-04 01:53:37 +0300

    [TASK-2-1] fix go fmt

--- kristina.lotonina/task-2-1/cmd/service/main.go
+++ kristina.lotonina/task-2-1/cmd/service/main.go
@@ -8,42 +8,47 @@ type Value struct {
 	temperature int
 }
 
+const (
+	MaxTemp = 30
+	MinTemp = 15
+)
+
 func TValues() Value {
 	return Value{
-		higher:      30,
-		lower:       15,
+		higher:      MaxTemp,
+		lower:       MinTemp,
 		temperature: 0,
 	}
 }
 
 func (values *Value) UpdateValues(operation string, temp int) {
 	switch operation {
-		case ">=":
-			if temp > values.lower {
-				values.lower = temp
-			}
-			if temp > values.temperature {
-				values.temperature = temp
-			}
-		case "<=":
-			if temp < values.higher {
-				values.higher = temp
-			}
-			if temp < values.temperature {
-				fmt.Print(-1)
-
-				return
-			}
-		default:
-			fmt.Println("undefined operation")
+	case ">=":
+		if temp > values.lower {
+			values.lower = temp
+		}
+		if temp > values.temperature {
+			values.temperature = temp
+		}
+	case "<=":
+		if temp < values.higher {
+			values.higher = temp
+		}
+		if temp < values.temperature {
+			fmt.Print(-1)
 
 			return
 		}
+	default:
+		fmt.Println("undefined operation")
+
+		return
+	}
 }
 
 func FindTemp(count int) {
 	var operation string
-	var temp 	  int
+	var temp int
 	values := TValues()
 	for range count {
 		fmt.Scanln(&operation, &temp)
@@ -59,7 +64,7 @@ func FindTemp(count int) {
 			return
 		}
 	}
-	fmt.Println(values.temperature)	
+	fmt.Println(values.temperature)
 }
 
 func main() {

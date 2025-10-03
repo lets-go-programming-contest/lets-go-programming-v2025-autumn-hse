05b1276573f35eea213b9fd57d869a2ff906385f
kristina <kristinalotonina@yandex.ru>
2025-10-04 02:01:57 +0300

    [TASK-2-1] fix cuddled

--- kristina.lotonina/task-2-1/cmd/service/main.go
+++ kristina.lotonina/task-2-1/cmd/service/main.go
@@ -24,22 +24,27 @@ func TValues() Value {
 func (values *Value) UpdateValues(operation string, temp int) {
 	switch operation {
 	case ">=":
+
 		if temp > values.lower {
 			values.lower = temp
 		}
+
 		if temp > values.temperature {
 			values.temperature = temp
 		}
 	case "<=":
+
 		if temp < values.higher {
 			values.higher = temp
 		}
+
 		if temp < values.temperature {
 			fmt.Print(-1)
 
 			return
 		}
 	default:
+		
 		fmt.Println("undefined operation")
 
 		return
@@ -57,17 +62,20 @@ func FindTemp(count int) {
 	for range count {
 		fmt.Scanln(&operation, &temp)
 		values.UpdateValues(operation, temp)
+		
 		if values.temperature > values.higher {
 			fmt.Print(-1)
 
 			return
 		}
+
 		if values.temperature < values.lower {
 			fmt.Print(-1)
 
 			return
 		}
 	}
+
 	fmt.Println(values.temperature)
 }
 
@@ -77,6 +85,7 @@ func main() {
 	)
 
 	fmt.Scan(&number, &count)
+	
 	for range number {
 		FindTemp(count)
 	}

52f8ac6b9b06ff2aafd634688c8c4db2a6609950
kristina <kristinalotonina@yandex.ru>
2025-10-04 03:29:17 +0300

    [TASK-2-1] add new file in internal

--- kristina.lotonina/task-2-1/cmd/service/main.go
+++ kristina.lotonina/task-2-1/cmd/service/main.go
@@ -1,66 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type Value struct {
-	higher int
-	lower  int
-}
-
-const (
-	MaxTemp = 30
-	MinTemp = 15
+	"github.com/kef1rch1k/task-2-1/internal/temperature"
 )
-
-func TValues() Value {
-	return Value{
-		higher: MaxTemp,
-		lower:  MinTemp,
-	}
-}
-
-func (values *Value) UpdateValues(operation string, temp int) {
-	switch operation {
-	case ">=":
-		if temp > values.lower {
-			values.lower = temp
-		}
-	case "<=":
-		if temp < values.higher {
-			values.higher = temp
-		}
-	default:
-		fmt.Println("undefined operation")
-
-		return
-	}
-}
-
-func FindTemp(count int) {
-	var (
-		operation string
-		temp      int
-	)
-
-	values := TValues()
-
-	for range count {
-		_, err := fmt.Scanln(&operation, &temp)
-		if err != nil {
-			fmt.Println("unable to read")
-
-			return
-		}
-
-		values.UpdateValues(operation, temp)
-
-		if values.lower <= values.higher {
-			fmt.Println(values.lower)
-		} else {
-			fmt.Println(-1)
-		}
-	}
-}
 
 func main() {
 	var number, count int

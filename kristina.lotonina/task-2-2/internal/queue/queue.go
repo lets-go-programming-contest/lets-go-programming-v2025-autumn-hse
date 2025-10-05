e7bc7ac18999d9c845f4b4169ea39b8b9128d9cf
kristina <kristinalotonina@yandex.ru>
2025-10-06 01:23:04 +0300

    [TASK-2-2] use pointer receiver in queue

--- kristina.lotonina/task-2-2/internal/queue/queue.go
+++ kristina.lotonina/task-2-2/internal/queue/queue.go
@@ -2,16 +2,16 @@ package dishes
 
 type Heap []int
 
-func (dishes Heap) Len() int {
-	return len(dishes)
+func (dishes *Heap) Len() int {
+	return len(*dishes)
 }
 
-func (dishes Heap) Less(i int, j int) bool {
-	return dishes[i] > dishes[j]
+func (dishes *Heap) Less(i int, j int) bool {
+	return (*dishes)[i] > (*dishes)[j]
 }
 
-func (dishes Heap) Swap(i int, j int) {
-	dishes[i], dishes[j] = dishes[j], dishes[i]
+func (dishes *Heap) Swap(i int, j int) {
+	(*dishes)[i], (*dishes)[j] = (*dishes)[j], (*dishes)[i]
 }
 
 func (dishes *Heap) Push(x interface{}) {
@@ -23,7 +23,7 @@ func (dishes *Heap) Push(x interface{}) {
 func (dishes *Heap) Pop() interface{} {
 	old := *dishes
 	n := len(old)
-	if (n == 0) {
+	if n == 0 {
 		return nil
 	}
 	x := old[n-1]

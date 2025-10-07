ec628e119c0d1753acc3d3e72e5abba599b09ab0
Ekaterina-101 <mnm54637@gmail.com>
2025-10-07 17:10:44 +0300

    [TASK-2-2] Fix pointer and error

--- ekaterina.kuznetsova/task-2-2/internal/myheap/heap.go
+++ ekaterina.kuznetsova/task-2-2/internal/myheap/heap.go
@@ -1,28 +1,23 @@
 package myheap
 
-import "errors"
-
-var errCast = errors.New("cast error")
-
-//nolint:recvcheck // is linked type
 type IntHeap []int
 
-func (h IntHeap) Len() int {
-	return len(h)
+func (h *IntHeap) Len() int {
+	return len(*h)
 }
 
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] > h[j]
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j]
 }
 
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *IntHeap) Push(x any) {
 	value, ok := x.(int)
 	if !ok {
-		panic(errCast)
+		panic("cast to int error")
 	}
 
 	*h = append(*h, value)

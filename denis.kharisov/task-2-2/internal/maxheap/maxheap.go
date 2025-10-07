d08074dc3b71a7a1cc9056021acc2dfb6b80c3af
denisK-H <fjk5940@gmail.com>
2025-10-07 15:49:50 +0300

    [TASK-2-2] first try

---
+++ denis.kharisov/task-2-2/internal/maxheap/maxheap.go
@@ -0,0 +1,27 @@
+package maxheap
+
+type Maxheap []int
+
+func (heap Maxheap) Len() int {
+	return len(heap);
+}
+
+func (heap Maxheap) Less(i, j int) bool {
+	return heap[i] > heap[j]
+}
+
+func (heap Maxheap) Swap(i, j int) {
+	heap[i], heap[j] = heap[j], heap[i]
+}
+
+func (heap* Maxheap) Push(x any) {
+	*heap = append(*heap, x.(int))
+}
+
+func (heap* Maxheap) Pop() any {
+	old := *heap
+	lenOld := len(old)
+	lastElem := old[lenOld - 1]
+	*heap = old[0 : lenOld - 1]
+	return lastElem
+}

bbef1239057354493d0816a303b569c13b9c63ac
denisK-H <fjk5940@gmail.com>
2025-10-07 16:46:38 +0300

    [TASK-2-2] fix format

--- denis.kharisov/task-2-2/internal/maxheap/maxheap.go
+++ denis.kharisov/task-2-2/internal/maxheap/maxheap.go
@@ -1,28 +1,34 @@
 package maxheap
 
+import "fmt"
+
 type Maxheap []int
 
-func (heap* Maxheap) Len() int {
+func (heap *Maxheap) Len() int {
 	return len(*heap)
 }
 
-func (heap* Maxheap) Less(i, j int) bool {
+func (heap *Maxheap) Less(i, j int) bool {
 	return (*heap)[i] > (*heap)[j]
 }
 
-func (heap* Maxheap) Swap(i, j int) {
+func (heap *Maxheap) Swap(i, j int) {
 	(*heap)[i], (*heap)[j] = (*heap)[j], (*heap)[i]
 }
 
-func (heap* Maxheap) Push(x any) {
-	*heap = append(*heap, x.(int))
+func (heap *Maxheap) Push(x any) {
+	value, errFlag := x.(int)
+	if !errFlag {
+		fmt.Printf("MaxHeap.Push: expected int")
+	}
+	*heap = append(*heap, value)
 }
 
-func (heap* Maxheap) Pop() any {
+func (heap *Maxheap) Pop() any {
 	old := *heap
 	lenOld := len(old)
-	lastElem := old[lenOld - 1]
-	*heap = old[0 : lenOld - 1]
+	lastElem := old[lenOld-1]
+	*heap = old[0 : lenOld-1]
 
 	return lastElem
 }

7f8067e4e934b52cd7660a8b37eb06252fb46c66
kamilSharipov <kamil5812@gmail.com>
2025-10-09 12:50:01 +0300

    [TASK-3] linter

--- kamil.sharipov/task-3/internal/json_formatter/json_formatter.go
+++ kamil.sharipov/task-3/internal/json_formatter/json_formatter.go
@@ -18,15 +18,17 @@ type ValuteJSON struct {
 
 func parseValue(s string) (float64, error) {
 	s = strings.Replace(s, ",", ".", 1)
+
 	return strconv.ParseFloat(s, 64)
 }
 
 func FormateJSON(valCurs *xml.ValCurs) ([]byte, error) {
 	valutes := make([]ValuteJSON, len(valCurs.Valutes))
-	for i, valute := range valCurs.Valutes {
+	for index, valute := range valCurs.Valutes {
 		numCode := 0
 		if valute.NumCode != "" {
 			var err error
+
 			numCode, err = strconv.Atoi(valute.NumCode)
 			if err != nil {
 				return nil, fmt.Errorf("invalid numCode %q: %w", valute.NumCode, err)
@@ -36,17 +38,19 @@ func FormateJSON(valCurs *xml.ValCurs) ([]byte, error) {
 		charCode := valute.CharCode
 
 		value := 0.0
+
 		if valute.Value != "" {
 			var err error
 			value, err = parseValue(valute.Value)
+
 			if err != nil {
 				return nil, fmt.Errorf("invalid Value %q: %w", valute.Value, err)
 			}
 		}
 
-		valutes[i].CharCode = charCode
-		valutes[i].NumCode = numCode
-		valutes[i].Value = value
+		valutes[index].CharCode = charCode
+		valutes[index].NumCode = numCode
+		valutes[index].Value = value
 	}
 
 	sort.Slice(valutes, func(i, j int) bool {

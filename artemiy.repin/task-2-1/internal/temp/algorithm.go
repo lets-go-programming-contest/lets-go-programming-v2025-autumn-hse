5f438ff1c17fc8d8238b278396ba81ba9061cf82
Nevermind0911 <repinarti@gmail.com>
2025-10-10 18:35:11 +0300

    [TASK-2-1] third linter fix

--- artemiy.repin/task-2-1/internal/temp/algorithm.go
+++ artemiy.repin/task-2-1/internal/temp/algorithm.go
@@ -5,39 +5,55 @@ const (
 	MaxBound = 30
 )
 
-func UpdateInterval(left int, right int, operator string, value int) (int, int) {
-	switch operator {
-		case "<=":
-			if value < right {
-				right = value
-			}
-		case ">=":
-			if value > left {
-				left = value
-			}
-		default:
-
-			return MinBound, MaxBound
-	}
-
-	return left, right
+type Temperature struct {
+	LeftBound  int
+	RightBound int
 }
 
-func GetOptimal(left int, right int) int {
-	if left > right {
+func UpdateTemperature() Temperature {
+	temperature := Temperature{
+		LeftBound:  MinBound,
+		RightBound: MaxBound,
+	}
 
+	return temperature
+}
+
+func (t *Temperature) UpdateInterval(operator string, value int) {
+	switch operator {
+	case "<=":
+		if value < t.RightBound {
+			t.RightBound = value
+		}
+
+	case ">=":
+		if value > t.LeftBound {
+			t.LeftBound = value
+		}
+
+	default:
+		t.LeftBound = MinBound
+		t.RightBound = MaxBound
+	}
+}
+
+func (t *Temperature) GetOptimal() int {
+	left := t.LeftBound
+	right := t.RightBound
+
+	if left > right {
 		return -1
 	}
 
 	if left < MinBound {
 		left = MinBound
 	}
+
 	if right > MaxBound {
 		right = MaxBound
 	}
 
 	if left > right {
-
 		return -1
 	}
 

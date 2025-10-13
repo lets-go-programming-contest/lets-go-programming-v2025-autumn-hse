14c5dbdbdd74e703c8d9a8c07e08e77220679009
kristina <kristinalotonina@yandex.ru>
2025-10-14 00:30:21 +0300

    [TASK-2-1] changes

--- kristina.lotonina/task-2-1/cmd/service/main.go
+++ kristina.lotonina/task-2-1/cmd/service/main.go
@@ -6,6 +6,18 @@ import (
 	"github.com/kef1rch1k/task-2-1/internal/temperature"
 )
 
+func FindTemp(values *temperature.Value, operation string, temp int) (int, error) {
+	err := values.UpdateValues(operation, temp)
+	if err != nil {
+		return 0, err
+	}
+
+	if values.Lower <= values.Higher {
+		return values.Lower, nil
+	}
+	return -1, nil
+}
+
 func main() {
 	var number, count int
 
@@ -20,10 +32,29 @@ func main() {
 		_, err = fmt.Scan(&count)
 		if err != nil {
 			fmt.Println("unable to read:", err)
-
 			return
 		}
 
-		temperature.FindTemp(count)
+		values := temperature.NewValues()
+
+		for j := 0; j < count; j++ {
+			var (
+				operation string
+				temp      int
+			)
+
+			_, err := fmt.Scanln(&operation, &temp)
+			if err != nil {
+				fmt.Println("unable to read:", err)
+				return
+			}
+
+			result, err := FindTemp(&values, operation, temp)
+			if err != nil {
+				fmt.Println("error:", err)
+				return
+			}
+			fmt.Println(result)
+		}
 	}
 }

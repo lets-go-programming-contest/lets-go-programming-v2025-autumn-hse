00307f276e07fe21adfe6d7dabff4daac7212b19
Ekaterina-101 <mnm54637@gmail.com>
2025-10-14 19:17:57 +0300

    [TASK-3] Fix json format

--- ekaterina.kuznetsova/task-3/cmd/service/main.go
+++ ekaterina.kuznetsova/task-3/cmd/service/main.go
@@ -7,9 +7,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"sort"
-	"strconv"
-	"strings"
 
 	"golang.org/x/net/html/charset"
 	"gopkg.in/yaml.v3"
@@ -20,20 +17,20 @@ type Config struct {
 	OutputFile string `yaml:"output-file"`
 }
 
-type Valute struct {
-	NumCode  string `xml:"NumCode"`
-	CharCode string `xml:"CharCode"`
-	Value    string `xml:"Value"`
-}
+// type Valute struct {
+// 	NumCode  string `xml:"NumCode"`
+// 	CharCode string `xml:"CharCode"`
+// 	Value    string `xml:"Value"`
+// }
 
 type ValuteCurs struct {
 	Valutes []Valute `xml:"Valute"`
 }
 
-type ValuteJSON struct {
-	NumCode  int     `json:"NumCode"`
-	CharCode string  `json:"CharCode"`
-	Value    float64 `json:"Value"`
+type Valute struct {
+	NumCode  int     `json:"NumCode"  xml:"NumCode"`
+	CharCode string  `json:"CharCode" xml:"CharCode"`
+	Value    float64 `json:"Value" xml:"Value"`
 }
 
 func LoadConfigYaml() (Config, error) {
@@ -78,40 +75,50 @@ func ParseValuteCursXML(inputFile string) (ValuteCurs, error) {
 	return valCurs, nil
 }
 
-func CreateValuteCursJSON(valCurs ValuteCurs) ([]byte, error) {
-	var valutesOutput []ValuteJSON
+// func CreateValuteCursJSON(valCurs ValuteCurs) ([]byte, error) {
+// 	var valutesOutput []Valute
 
-	for _, valute := range valCurs.Valutes {
-		numCode, _ := strconv.Atoi(valute.NumCode)
-		value, _ := strconv.ParseFloat(strings.ReplaceAll(valute.Value, ",", "."), 64)
-		valutesOutput = append(valutesOutput, ValuteJSON{
-			NumCode:  numCode,
-			CharCode: valute.CharCode,
-			Value:    value,
-		})
-	}
+// 	for _, valute := range valCurs.Valutes {
+// 		numCode, _ := strconv.Atoi(valute.NumCode)
+// 		value, _ := strconv.ParseFloat(strings.ReplaceAll(valute.Value, ",", "."), 64)
+// 		valutesOutput = append(valutesOutput, Valute{
+// 			NumCode:  numCode,
+// 			CharCode: valute.CharCode,
+// 			Value:    value,
+// 		})
+// 	}
 
-	sort.Slice(valutesOutput, func(i, j int) bool {
-		return valutesOutput[i].Value > valutesOutput[j].Value
-	})
+// 	sort.Slice(valutesOutput, func(i, j int) bool {
+// 		return valutesOutput[i].Value > valutesOutput[j].Value
+// 	})
 
-	outputJSON, err := json.MarshalIndent(valutesOutput, "", "    ")
-	if err != nil {
-		return nil, fmt.Errorf("error marshaling JSON: %w", err)
-	}
+// 	outputJSON, err := json.MarshalIndent(valutesOutput, "", "    ")
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error marshaling JSON: %w", err)
+// 	}
 
-	return outputJSON, nil
-}
+// 	return outputJSON, nil
+// }
 
-func WriteFileJSON(outputFile string, outputJSON []byte) error {
+func WriteFileJSON(outputFile string, value any) error {
 	err := os.MkdirAll(filepath.Dir(outputFile), 0766)
 	if err != nil {
 		return fmt.Errorf("error creating directory: %w", err)
 	}
 
-	err = os.WriteFile(outputFile, outputJSON, 0644)
+	file, err := os.Create(outputFile)
 	if err != nil {
-		return fmt.Errorf("error writing output file: %w", err)
+		return fmt.Errorf("error open output file: %w", err)
+	}
+
+	defer func() {
+		file.Close()
+	}()
+
+	dataJSON := json.NewEncoder(file)
+	err = dataJSON.Encode(&value)
+	if err != nil {
+		return fmt.Errorf("erorr json encode  %w", err)
 	}
 
 	return nil
@@ -128,12 +135,12 @@ func main() {
 		panic(err)
 	}
 
-	outputJSON, err := CreateValuteCursJSON(valCurs)
-	if err != nil {
-		panic(err)
-	}
+	// outputJSON, err := CreateValuteCursJSON(valCurs)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	err = WriteFileJSON(config.OutputFile, outputJSON)
+	err = WriteFileJSON(config.OutputFile, valCurs.Valutes)
 	if err != nil {
 		panic(err)
 	}

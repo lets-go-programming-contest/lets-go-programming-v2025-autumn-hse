d0ebe0e9b35f7004372dfbda1b9592dc69f2aae8
Vanya <kuzid.24@gmail.com>
2025-10-14 18:25:12 +0300

    [TASK-3] reasonable change of variable type

--- ivan.kuznetsov/task-3/internal/jsonwriter/writer.go
+++ ivan.kuznetsov/task-3/internal/jsonwriter/writer.go
@@ -5,13 +5,12 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
 
 	"github.com/kuzid-17/task-3/internal/xmlparser"
 )
 
 type OutputValute struct {
-	NumCode  int     `json:"num_code"`
+	NumCode  string  `json:"num_code"`
 	CharCode string  `json:"char_code"`
 	Value    float64 `json:"value"`
 }
@@ -19,7 +18,7 @@ type OutputValute struct {
 func WriteJSON(filename string, valutes []xmlparser.Valute) {
 	dir := filepath.Dir(filename)
 
-	err := os.MkdirAll(dir, 0755)
+	err := os.MkdirAll(dir, 0o755)
 	if err != nil {
 		panic("failed to create directory")
 	}
@@ -27,13 +26,8 @@ func WriteJSON(filename string, valutes []xmlparser.Valute) {
 	output := make([]OutputValute, 0, len(valutes))
 
 	for _, valute := range valutes {
-		numCode, err := strconv.Atoi(valute.NumCode)
-		if err != nil {
-			continue
-		}
-
 		output = append(output, OutputValute{
-			NumCode:  numCode,
+			NumCode:  valute.NumCode,
 			CharCode: valute.CharCode,
 			Value:    float64(valute.Value),
 		})

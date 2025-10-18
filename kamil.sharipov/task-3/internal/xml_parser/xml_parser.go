de7f8fb1801e1b223725d6c580fcaade1572dabe
kamilSharipov <kamil5812@gmail.com>
2025-10-18 19:49:31 +0300

    [TASK-3] fix

--- kamil.sharipov/task-3/internal/xml_parser/xml_parser.go
+++ kamil.sharipov/task-3/internal/xml_parser/xml_parser.go
@@ -9,38 +9,29 @@ import (
 	"golang.org/x/net/html/charset"
 )
 
-type ValCurs struct {
-	XMLName xml.Name `xml:"ValCurs"`
-	Date    string   `xml:"Date,attr"`
-	Name    string   `xml:"name,attr"`
-	Valutes []Valute `xml:"Valute"`
-}
-
 type Valute struct {
-	XMLName   xml.Name `xml:"Valute"`
-	ID        string   `xml:"ID,attr"`
-	NumCode   string   `xml:"NumCode"`
-	CharCode  string   `xml:"CharCode"`
-	Nominal   int      `xml:"Nominal"`
-	Name      string   `xml:"Name"`
-	Value     string   `xml:"Value"`
-	VunitRate string   `xml:"VunitRate"`
+	NumCode  string `xml:"NumCode"`
+	CharCode string `xml:"CharCode"`
+	Value    string `xml:"Value"`
 }
 
 var errNoValutes = errors.New("XML contains no valutes")
 
-func ParseXML(xmlData []byte) (*ValCurs, error) {
+func ParseXML(xmlData []byte) ([]Valute, error) {
 	xmlDecoder := xml.NewDecoder(bytes.NewReader(xmlData))
 	xmlDecoder.CharsetReader = charset.NewReaderLabel
 
-	var valCurs ValCurs
-	if err := xmlDecoder.Decode(&valCurs); err != nil {
+	var temp struct {
+		Valutes []Valute `xml:"Valute"`
+	}
+
+	if err := xmlDecoder.Decode(&temp); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal XML: %w", err)
 	}
 
-	if len(valCurs.Valutes) == 0 {
+	if len(temp.Valutes) == 0 {
 		return nil, errNoValutes
 	}
 
-	return &valCurs, nil
+	return temp.Valutes, nil
 }

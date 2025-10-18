71eaff019e0017899bb120c3db4f9a2119cc369b
German Feskov <g.feskov@yandex.ru>
2025-10-18 23:08:03 +0300

    [TASK-3] Fix: validate cfg

--- german.feskov/task-3/internal/config/config.go
+++ german.feskov/task-3/internal/config/config.go
@@ -4,31 +4,61 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/yaml.v3"
 )
 
-var errInvalidArgs error = errors.New("empty path")
+var (
+	ErrEmptyInput  = errors.New("input-file is required")
+	ErrEmptyOutput = errors.New("output-file is required")
+	ErrSamePath    = errors.New("input-file and output-file must differ")
+)
 
 type Config struct {
 	Input  string `yaml:"input-file"`
 	Output string `yaml:"output-file"`
 }
 
-func Load(filePath string) (Config, error) {
-	var cfg Config
-
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		return cfg, fmt.Errorf("read file %q: %w", filePath, err)
+func (c Config) Validate() error {
+	if c.Input == "" {
+		return ErrEmptyInput
 	}
 
+	if c.Output == "" {
+		return ErrEmptyOutput
+	}
+
+	inAbs, err := filepath.Abs(c.Input)
+	if err != nil {
+		return fmt.Errorf("could not get the absolute path for the input-file: %w", err)
+	}
+
+	outAbs, err := filepath.Abs(c.Output)
+	if err != nil {
+		return fmt.Errorf("could not get the absolute path for the output-file: %w", err)
+	}
+
+	if inAbs == outAbs {
+		return ErrSamePath
+	}
+
+	return nil
+}
+
+func Load(filePath string) (Config, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return Config{}, fmt.Errorf("read file %q: %w", filePath, err)
+	}
+
+	var cfg Config
 	if err := yaml.Unmarshal(data, &cfg); err != nil {
 		return cfg, fmt.Errorf("unmarshal from %q: %w", filePath, err)
 	}
 
-	if cfg.Input == "" || cfg.Output == "" {
-		return cfg, fmt.Errorf("unmarshal from %q: %w", filePath, errInvalidArgs)
+	if err := cfg.Validate(); err != nil {
+		return cfg, fmt.Errorf("unmarshal from %q: %w", filePath, err)
 	}
 
 	return cfg, nil

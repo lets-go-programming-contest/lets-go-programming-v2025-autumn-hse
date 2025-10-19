8adffb4e78203dc82f13dd04880b1d0b97fa2f13
German Feskov <g.feskov@yandex.ru>
2025-10-19 21:01:23 +0300

    [TASK-3] Add: validation

--- german.feskov/task-3/internal/config/config.go
+++ german.feskov/task-3/internal/config/config.go
@@ -1,49 +1,16 @@
 package config
 
 import (
-	"errors"
 	"fmt"
 	"os"
-	"path/filepath"
 
+	"github.com/go-playground/validator/v10"
 	"gopkg.in/yaml.v3"
 )
 
-var (
-	ErrEmptyInput  = errors.New("input-file is required")
-	ErrEmptyOutput = errors.New("output-file is required")
-	ErrSamePath    = errors.New("input-file and output-file must differ")
-)
-
 type Config struct {
-	Input  string `yaml:"input-file"`
-	Output string `yaml:"output-file"`
-}
-
-func (c Config) Validate() error {
-	if c.Input == "" {
-		return ErrEmptyInput
-	}
-
-	if c.Output == "" {
-		return ErrEmptyOutput
-	}
-
-	inAbs, err := filepath.Abs(c.Input)
-	if err != nil {
-		return fmt.Errorf("could not get the absolute path for the input-file: %w", err)
-	}
-
-	outAbs, err := filepath.Abs(c.Output)
-	if err != nil {
-		return fmt.Errorf("could not get the absolute path for the output-file: %w", err)
-	}
-
-	if inAbs == outAbs {
-		return ErrSamePath
-	}
-
-	return nil
+	Input  string `validate:"filepath" yaml:"input-file"`
+	Output string `validate:"filepath" yaml:"output-file"`
 }
 
 func Load(filePath string) (Config, error) {
@@ -57,8 +24,9 @@ func Load(filePath string) (Config, error) {
 		return cfg, fmt.Errorf("unmarshal from %q: %w", filePath, err)
 	}
 
-	if err := cfg.Validate(); err != nil {
-		return cfg, fmt.Errorf("unmarshal from %q: %w", filePath, err)
+	validate := validator.New()
+	if err := validate.Struct(&cfg); err != nil {
+		return cfg, fmt.Errorf("validate config from %q: %w", filePath, err)
 	}
 
 	return cfg, nil

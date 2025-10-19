cddabed85bc6f91bac2ea8181ba0ea28b062d41b
User <dvrogov06@yandex.ru>
2025-10-19 20:17:30 +0300

    [TASK-3] rewrite main and cfg reader

--- danil.rogov/task-3/internal/config/config.go
+++ danil.rogov/task-3/internal/config/config.go
@@ -12,6 +12,14 @@ type Config struct {
 }
 
 func LoadConfig(path string) (*Config, error) {
+	if path == "" {
+		return nil, &os.PathError{Op: "open", Path: "", Err: os.ErrNotExist}
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil, err
+	}
+
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
@@ -21,6 +29,13 @@ func LoadConfig(path string) (*Config, error) {
 	err = yaml.Unmarshal(data, &config)
 	if err != nil {
 		return nil, err
+	}
+
+	if config.InputFile == "" {
+		return nil, &os.PathError{Op: "open", Path: "input_file", Err: os.ErrInvalid}
+	}
+	if config.OutputFile == "" {
+		return nil, &os.PathError{Op: "open", Path: "output_file", Err: os.ErrInvalid}
 	}
 
 	return &config, nil

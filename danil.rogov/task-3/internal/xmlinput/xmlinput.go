f4c5f7dd1781fd451ba9572b784074f5732ca41c
User <dvrogov06@yandex.ru>
2025-10-19 20:03:08 +0300

    [TASK-3] new func for xml

--- danil.rogov/task-3/internal/xmlinput/xmlinput.go
+++ danil.rogov/task-3/internal/xmlinput/xmlinput.go
@@ -13,19 +13,25 @@ import (
 )
 
 func ReadXML(path string) ([]valute.ValuteInfo, error) {
+	// Check if input file exists
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil, err
+	}
+
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, fmt.Errorf("open file: %w", err)
 	}
-
 	defer file.Close()
 
 	decoder := xml.NewDecoder(file)
 	decoder.CharsetReader = func(charset string, input io.Reader) (io.Reader, error) {
-		if charset == "windows-1251" {
+		switch strings.ToLower(charset) {
+		case "windows-1251":
 			return charmap.Windows1251.NewDecoder().Reader(input), nil
+		default:
+			return input, nil
 		}
-		return nil, fmt.Errorf("unknown charset: %s", charset)
 	}
 
 	var valCurs valute.ValCurs
@@ -35,9 +41,12 @@ func ReadXML(path string) ([]valute.ValuteInfo, error) {
 
 	result := make([]valute.ValuteInfo, 0, len(valCurs.Valutes))
 	for _, v := range valCurs.Valutes {
-		value, _ := strconv.ParseFloat(strings.ReplaceAll(v.Value, ",", "."), 64)
+		valueStr := strings.ReplaceAll(v.Value, ",", ".")
+		value, err := strconv.ParseFloat(valueStr, 64)
+		if err != nil {
+			return nil, fmt.Errorf("parse value %s: %w", v.Value, err)
+		}
 		numcode, _ := strconv.Atoi(v.NumCode)
-
 		result = append(result, valute.ValuteInfo{
 			NumCode:  numcode,
 			CharCode: v.CharCode,

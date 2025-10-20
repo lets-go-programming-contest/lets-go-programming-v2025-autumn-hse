bd7134848ddc105f9de4a75a8d061549b50c563a
kristina <kristinalotonina@yandex.ru>
2025-10-20 14:48:30 +0300

    [TASK-3] errors %w

--- kristina.lotonina/task-3/internal/config/config.go
+++ kristina.lotonina/task-3/internal/config/config.go
@@ -14,13 +14,12 @@ type Config struct {
 func LoadConfig(path string) (*Config, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("reading config file: %w", err)
 	}
 
 	var cfg Config
-	err = yaml.Unmarshal(data, &cfg)
-	if err != nil {
-		return nil, err
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, fmt.Errorf("unmarshalling yaml: %w", err)
 	}
 
 	return &cfg, nil

ac22c04dfc72842174d5f007958b3a69bceebace
Nevermind0911 <repinarti@gmail.com>
2025-10-21 17:56:44 +0300

    [TASK-2-2] first commit

---
+++ artemiy.repin/task-2-2/internal/heap/heap.go
@@ -0,0 +1,37 @@
+package heap
+
+type MinHeap []int
+
+func (h MinHeap) Len() int {
+	return len(h)
+}
+
+func (h MinHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h MinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MinHeap) Push(value any) {
+	_value, ok := value.(int)
+
+	if !ok {
+		panic("Need to push int")
+	}
+
+	*h = append(*h, _value)
+}
+
+func (h *MinHeap) Pop() any {
+	length := len(*h)
+
+	if length == 0 {
+		panic("Pop from empty heap")
+	}
+
+	x := (*h)[length-1]
+	*h = (*h)[:length-1]
+	return x
+}

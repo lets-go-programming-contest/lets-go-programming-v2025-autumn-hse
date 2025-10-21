67b4c91ebe6c1b8a4dd0b7fca39ce566dbce703b
bamartynov <bamartynov@edu.hse.ru>
2025-10-21 17:39:10 +0300

    [TASK-3] fix4

--- boris.martynov/task-3/internal/valuteinfo/valuteinfo.go
+++ boris.martynov/task-3/internal/valuteinfo/valuteinfo.go
@@ -1,13 +1,40 @@
 package valuteinfo
 
-type value float64
+import (
+	"encoding/xml"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Value float64
 
 type Valute struct {
 	NumCode  int    `xml:"NumCode" json:"NumCode"`
 	CharCode string `xml:"CharCode" json:"CharCode"`
-	Value    value  `xml:"Value" json:"Value"`
+	Value    Value  `xml:"Value" json:"Value"`
 }
 
 type ValuteCurs struct {
 	Valutes []Valute `xml:"Valute"`
+}
+
+func (v *Value) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var str string
+
+	if err := d.DecodeElement(&str, &start); err != nil {
+
+		return fmt.Errorf("while decoding: %w", err)
+	}
+
+	value, err := strconv.ParseFloat(strings.ReplaceAll(str, ",", "."), 64)
+
+	if err != nil {
+
+		return fmt.Errorf("while replacing dots: %w", err)
+	}
+
+	*v = Value(value)
+
+	return nil
 }

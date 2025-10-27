3e2b9af4894e573e6e34e2fe3527407aff04c81c
Ekaterina-101 <mnm54637@gmail.com>
2025-10-27 21:41:01 +0300

    [TASK-3] Fix parseXML and config

---
+++ ekaterina.kuznetsova/task-3/internal/config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+func LoadConfigYaml(configPath *string) (Config, error) {
+	dataConfig, err := os.ReadFile(*configPath)
+	if err != nil {
+		return Config{}, fmt.Errorf("error read config: %w", err)
+	}
+
+	var config Config
+
+	err = yaml.Unmarshal(dataConfig, &config)
+	if err != nil {
+		return Config{}, fmt.Errorf("error parsing yaml: %w", err)
+	}
+
+	return config, nil
+}

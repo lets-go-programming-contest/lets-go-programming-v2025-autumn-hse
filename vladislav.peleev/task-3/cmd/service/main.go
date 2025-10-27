ec97d7b9ec5c800c538bdf5ab3c566ba73357543
Vladislav <mainmail30@bk.ru>
2025-10-27 15:35:52 -0400

    [TASK-3] Initial commit : Attempt 4

--- vladislav.peleev/task-3/cmd/service/main.go
+++ vladislav.peleev/task-3/cmd/service/main.go
@@ -21,9 +21,9 @@ type Config struct {
 }
 
 type Currency struct {
-	NumCode  string  `xml:"NumCode"`
-	CharCode string  `xml:"CharCode"`
-	Value    string  `xml:"Value"`
+	NumCode  string `xml:"NumCode"`
+	CharCode string `xml:"CharCode"`
+	Value    string `xml:"Value"`
 }
 
 type ValCurs struct {

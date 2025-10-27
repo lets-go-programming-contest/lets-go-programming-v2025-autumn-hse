df71d7831dc1f41a73f8dd0ec6de2305bf90b460
OlesiaOl <olesia.olshv@gmail.com>
2025-10-28 01:19:21 +0300

    [TASK-3] writer 05

--- olesia.olshevskaia/task-3/internal/currency/writer.go
+++ olesia.olshevskaia/task-3/internal/currency/writer.go
@@ -4,31 +4,36 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
+
+	"github.com/Olesia.Ol/task-3/internal/model"
 )
 
-func WriteJSON(currencies interface{}, outputPath string) error {
-	dir := filepath.Dir(outputPath)
-	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+func ParseJSON[T any](outputFile string, data T, dirmode, filemode os.FileMode) error {
+
+	if err := os.MkdirAll(filepath.Dir(outputFile), dirmode); err != nil {
 		return err
 	}
 
-	file, err := os.Create(outputPath)
+	file, err := os.Create(outputFile)
 	if err != nil {
 		return err
 	}
 
 	defer func() {
-		if err := file.Close(); err != nil {
-			panic("Error closing XML file: " + err.Error())
+		if cerr := file.Close(); cerr != nil {
+			panic("Error closing JSON file: " + cerr.Error())
 		}
 	}()
 
 	encoder := json.NewEncoder(file)
 	encoder.SetIndent("", "  ")
-
-	if err := encoder.Encode(currencies); err != nil {
+	if err := encoder.Encode(data); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func WriteJSON(outputFile string, currencies []model.Currency, dirmode, filemode os.FileMode) error {
+	return ParseJSON(outputFile, currencies, dirmode, filemode)
 }

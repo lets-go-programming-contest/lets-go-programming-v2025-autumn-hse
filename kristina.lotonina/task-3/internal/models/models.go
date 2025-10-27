ff62c4f1849e2b691ad1c87a9f044151ca0e64c8
kristina <kristinalotonina@yandex.ru>
2025-10-28 01:28:29 +0300

    [TASK-3] one struct

--- kristina.lotonina/task-3/internal/models/models.go
+++ kristina.lotonina/task-3/internal/models/models.go
@@ -1,18 +1,36 @@
 package models
 
+import (
+	"encoding/xml"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type ValCurs struct {
 	Valutes []Valute `xml:"Valute"`
 }
 
 type Valute struct {
-	NumCode  int     `xml:"NumCode"`
-	CharCode string  `xml:"CharCode"`
-	ValueStr string  `xml:"Value"`
-	Value    float64 `xml:"-"`
+	NumCode  int        `json:"num_code"  xml:"NumCode"`
+	CharCode string     `json:"char_code" xml:"CharCode"`
+	Value    CommaFloat `json:"value"     xml:"Value"`
 }
 
-type OutputValute struct {
-	NumCode  int     `json:"num_code"`
-	CharCode string  `json:"char_code"`
-	Value    float64 `json:"value"`
+type CommaFloat float64
+
+func (cf *CommaFloat) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var s string
+	if err := d.DecodeElement(&s, &start); err != nil {
+		return err
+	}
+
+	val := strings.Replace(s, ",", ".", 1)
+	f, err := strconv.ParseFloat(strings.TrimSpace(val), 64)
+	if err != nil {
+		return fmt.Errorf("parsing CommaFloat '%s': %w", s, err)
+	}
+
+	*cf = CommaFloat(f)
+	return nil
 }

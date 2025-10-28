ee2db35aba1075ae8b7bc052924d5e6f6985a665
bamartynov <bamartynov@edu.hse.ru>
2025-10-28 12:38:31 +0300

    [TASK-3] linter

--- boris.martynov/task-3/internal/valuteinfo/valuteinfo.go
+++ boris.martynov/task-3/internal/valuteinfo/valuteinfo.go
@@ -10,9 +10,9 @@ import (
 type Value float64
 
 type Valute struct {
-	NumCode  int    `xml:"NumCode" json:"num_code"`
-	CharCode string `xml:"CharCode" json:"char_code"`
-	Value    Value  `xml:"Value" json:"value"`
+	NumCode  int    `json:"num_code" xml:"NumCode"`
+	CharCode string `json:"char_code" xml:"CharCode"`
+	Value    Value  `json:"value" xml:"Value"`
 }
 
 type ValuteCurs struct {
@@ -21,14 +21,14 @@ type ValuteCurs struct {
 
 func (v *Value) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	var str string
-
 	if err := d.DecodeElement(&str, &start); err != nil {
 
 		return fmt.Errorf("while decoding: %w", err)
 	}
-	str = strings.TrimSpace(str)
-	value, err := strconv.ParseFloat(strings.ReplaceAll(str, ",", "."), 64)
 
+	str = strings.TrimSpace(str)
+
+	value, err := strconv.ParseFloat(strings.ReplaceAll(str, ",", "."), 64)
 	if err != nil {
 
 		return fmt.Errorf("while replacing dots: %w", err)

40436ec2c7db8ce965f3a2f8363edd742c6b9957
OlesiaOl <olesia.olshv@gmail.com>
2025-11-11 15:56:04 +0300

    [TASK-2-2] (#34)
    * [TASK-2-2] Add solution for subtask 2
    
    * [TASK-2-2] fix lint1
    
    * [TASK-2-2] fix lint2
    
    * [TASK-2-2] fix lint3
    
    * [TASK-2-2] fix lint4
    
    * [TASK-2-2] fix lint6
    
    * [TASK-2-2] fix lint7
    
    * [TASK-2-2] intHeap is empty
    
    * [TASK-2-2] grooming
    
    * [TASK-2-2] panic
    
    * [TASK-2-2] grooming2
    
    * [TASK-2-2] boosting the economy
    
    * [TASK-2-2] grooming3
    
    * [TASK-2-2] errors grooming
    
    * [TASK-2-2] package
    
    * [TASK-2-2] package 2
    
    * [TASK-2-2] correction vars
    
    * [TASK-2-2] correction pop
    
    * [TASK-2-2] correction pop in main
    
    * [TASK-2-2] grooming
    
    * [TASK-2-2] comment to panic
    
    * [TASK-2-2] fix comment println
    
    * [TASK-2-2] grooming 2
    
    * [TASK-2-2] recover
    
    * [TASK-2-2] recover grooming
    
    * [TASK-2-2] grooming 3
    
    * [TASK-2-2] desiredDish
    
    * [TASK-2-2] mini main
    
    * [TASK-2-2] grooming
    
    * [TASK-2-2] grooming 04
    
    * [TASK-2-2] grooming 05
    
    * [TASK-2-2] folders
    
    * [TASK-2-2] index
    
    * [TASK-2-2] index 2
    
    * [TASK-2-2] grooming
    
    * [TASK-2-2] correction of comments ;)
    
    * [TASK-2-2] delete errors
    
    * [TASK-2-1] correction

---
+++ olesia.olshevskaia/task-2-2/cmd/service/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"container/heap"
+	"errors"
+	"fmt"
+
+	"github.com/olesia.olshevsraia/task-2-2/internal/intheap"
+)
+
+var (
+	ErrDesiredDishTooBig = errors.New("desired dish is greater than the number of dishes in the heap")
+	ErrHeapEmpty         = errors.New("heap is empty")
+	ErrInvalidType       = errors.New("invalid type from heap.Pop")
+)
+
+func popDesiredDish(heapInt intheap.IntHeap, desired int) (int, error) {
+	if desired > heapInt.Len() {
+		return 0, ErrDesiredDishTooBig
+	}
+
+	for range desired - 1 {
+		heap.Pop(&heapInt)
+	}
+
+	val := heap.Pop(&heapInt)
+	count, ok := val.(int)
+
+	if !ok {
+		return 0, ErrInvalidType
+	}
+
+	return count, nil
+}
+
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("Panic occurred: %v\n", r)
+		}
+	}()
+
+	myHeap := &intheap.IntHeap{}
+	heap.Init(myHeap)
+
+	var numberDishes int
+	if _, err := fmt.Scanln(&numberDishes); err != nil {
+		fmt.Println("error reading the number of dishes", err)
+
+		return
+	}
+
+	var dish int
+	for range numberDishes {
+		if _, err := fmt.Scan(&dish); err != nil {
+			fmt.Println("error reading dish", err)
+
+			return
+		}
+
+		heap.Push(myHeap, dish)
+	}
+
+	var desiredDish int
+	if _, err := fmt.Scanln(&desiredDish); err != nil {
+		fmt.Println("error reading priority", err)
+
+		return
+	}
+
+	result, err := popDesiredDish(*myHeap, desiredDish)
+	if err != nil {
+		fmt.Println("failed to get dish with given id:", err)
+
+		return
+	}
+
+	fmt.Println(result)
+}
---
+++ olesia.olshevskaia/task-2-2/go.mod
@@ -0,0 +1,3 @@
+module github.com/olesia.olshevsraia/task-2-2
+
+go 1.22.7

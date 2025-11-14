e5568a386349b00c2b368ce3296c92ed74ae2214
bamartynov <bamartynov@edu.hse.ru>
2025-11-14 15:09:37 +0300

    [TASK-3] changes_requested_2

--- boris.martynov/task-3/internal/xmlparser/xmlparser.go
+++ boris.martynov/task-3/internal/xmlparser/xmlparser.go
@@ -1,15 +1,12 @@
 package xmlparser
 
 import (
-	"bytes"
 	"encoding/xml"
 	"fmt"
-	"io"
 	"os"
-	"strings"
 
 	"github.com/JingolBong/task-3/internal/valuteinfo"
-	"golang.org/x/text/encoding/charmap"
+	"golang.org/x/net/html/charset"
 )
 
 func XMLParse(inputxml string) (valuteinfo.ValuteCurs, error) {
@@ -26,27 +23,14 @@ func XMLParse(inputxml string) (valuteinfo.ValuteCurs, error) {
 		}
 	}()
 
-	data, err := io.ReadAll(xmlFile)
+	reader, err := charset.NewReader(xmlFile, "")
 	if err != nil {
-		return valuteinfo.ValuteCurs{}, fmt.Errorf("reading file: %w", err)
+		return valuteinfo.ValuteCurs{}, fmt.Errorf("charset reader: %w", err)
 	}
 
-	if bytes.Contains(data, []byte("windows-1251")) {
-		decoder := charmap.Windows1251.NewDecoder()
-
-		data, err = decoder.Bytes(data)
-		if err != nil {
-			return valuteinfo.ValuteCurs{}, fmt.Errorf("cannot decode windows-1251: %w", err)
-		}
-	}
-
-	dataStr := strings.ReplaceAll(string(data), `encoding="windows-1251"`, `encoding="utf-8"`)
-
-	data = []byte(dataStr)
-
-	if err := xml.Unmarshal(data, &valuteCur); err != nil {
+	decoder := xml.NewDecoder(reader)
+	if err := decoder.Decode(&valuteCur); err != nil {
 		return valuteinfo.ValuteCurs{}, fmt.Errorf("unmarshal xml: %w", err)
 	}
-
 	return valuteCur, nil
 }

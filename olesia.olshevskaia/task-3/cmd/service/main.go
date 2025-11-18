db75780a089f2a54da08fd433ea00fb6868325bc
OlesiaOl <olesia.olshv@gmail.com>
2025-11-18 14:00:07 +0300

    [TASK-3] model 3

--- olesia.olshevskaia/task-3/cmd/service/main.go
+++ olesia.olshevskaia/task-3/cmd/service/main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
 	"github.com/Olesia.Ol/task-3/internal/config"
 	"github.com/Olesia.Ol/task-3/internal/currency"
 	"github.com/Olesia.Ol/task-3/internal/model"
 )
-
-type ValCurs struct {
-	Valutes []model.Currency `xml:"Valute"`
-}
 
 func main() {
 	configPath := flag.String("config", "config.yaml", "Path to YAML config file")
@@ -19,23 +14,19 @@ func main() {
 
 	cfg, err := config.Load(*configPath)
 	if err != nil {
-		fmt.Println("Cannot load config:", err)
-		return
+		panic("Cannot load config: " + err.Error())
 	}
 
-	valCurs, err := currency.Read[ValCurs](cfg.InputFile)
+	rates, err := currency.Read[model.CurrencyRates](cfg.InputFile)
 	if err != nil {
-		fmt.Println("Cannot read currencies:", err)
-		return
+		panic("Cannot read currencies: " + err.Error())
 	}
 
-	currencies := valCurs.Valutes
+	currencies := rates.Currencies
 
 	currency.Sort(currencies)
 
 	if err := currency.WriteJSON(cfg.OutputFile, currencies); err != nil {
-		fmt.Println("Cannot write JSON file:", err)
-		return
+		panic("Cannot write JSON file: " + err.Error())
 	}
-
 }
--- olesia.olshevskaia/task-3/internal/model/types.go
+++ olesia.olshevskaia/task-3/internal/model/types.go
@@ -1,23 +1,42 @@
 package model
 
+import (
+	"encoding/xml"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Config struct {
 	InputFile  string `yaml:"input_file"`
 	OutputFile string `yaml:"output_file"`
 }
 
+type CurrencyRates struct {
+	Currencies []Currency `xml:"Valute"`
+}
+
 type CurrencyValue float64
 
-//type Currency struct {
-//	CodeNum   int           `json:"num_code"  xml:"NumCode"`
-//	CodeChar  string        `json:"char_code" xml:"CharCode"`
-//	RateValue CurrencyValue `json:"value"     xml:"Value"`
-//}
-
 type Currency struct {
-	CodeNum   int     `json:"num_code"  xml:"-"`
-	CodeChar  string  `json:"char_code" xml:"CharCode"`
-	RateValue float64 `json:"value"     xml:"-"`
-	HasValue  bool    `json:"-"         xml:"-"`
-	RawNum    string  `xml:"NumCode"`
-	RawValue  string  `xml:"Value"`
+	CodeNum   int           `json:"num_code"  xml:"NumCode"`
+	CodeChar  string        `json:"char_code" xml:"CharCode"`
+	RateValue CurrencyValue `json:"value"     xml:"Value"`
+}
+
+func (v *CurrencyValue) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var str string
+	if err := d.DecodeElement(&str, &start); err != nil {
+		return fmt.Errorf("error decode xml: %w", err)
+	}
+
+	str = strings.ReplaceAll(str, ",", ".")
+
+	value, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return fmt.Errorf("error parse float: %w", err)
+	}
+
+	*v = CurrencyValue(value)
+	return nil
 }

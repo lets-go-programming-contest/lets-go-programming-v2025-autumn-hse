242c86630589056aa20c94c7d4e78f3e53c2d1bb
OlesiaOl <olesia.olshv@gmail.com>
2025-11-18 13:42:40 +0300

    [TASK-3] model 2

--- olesia.olshevskaia/task-3/internal/model/types.go
+++ olesia.olshevskaia/task-3/internal/model/types.go
@@ -7,8 +7,17 @@ type Config struct {
 
 type CurrencyValue float64
 
+//type Currency struct {
+//	CodeNum   int           `json:"num_code"  xml:"NumCode"`
+//	CodeChar  string        `json:"char_code" xml:"CharCode"`
+//	RateValue CurrencyValue `json:"value"     xml:"Value"`
+//}
+
 type Currency struct {
-	CodeNum   int           `json:"num_code"  xml:"NumCode"`
-	CodeChar  string        `json:"char_code" xml:"CharCode"`
-	RateValue CurrencyValue `json:"value"     xml:"Value"`
+	CodeNum   int     `json:"num_code"  xml:"-"`
+	CodeChar  string  `json:"char_code" xml:"CharCode"`
+	RateValue float64 `json:"value"     xml:"-"`
+	HasValue  bool    `json:"-"         xml:"-"`
+	RawNum    string  `xml:"NumCode"`
+	RawValue  string  `xml:"Value"`
 }

b79151bfa1d892bdbb14a59bc2ef08094802389e
OlesiaOl <olesia.olshv@gmail.com>
2025-11-18 10:28:16 +0300

    [TASK-3] syntax2

--- olesia.olshevskaia/task-3/internal/currency/writer.go
+++ olesia.olshevskaia/task-3/internal/currency/writer.go
@@ -1,10 +1,15 @@
 package currency
 
 import (
+	"fmt"
+
 	"github.com/Olesia.Ol/task-3/internal/jsonutil"
 	"github.com/Olesia.Ol/task-3/internal/model"
 )
 
 func WriteJSON(outputFile string, currencies []model.Currency) error {
-	return jsonutil.ParseJSON(outputFile, currencies, jsonutil.DirPerm, jsonutil.FilePerm)
+	if err := jsonutil.ParseJSON(outputFile, currencies, jsonutil.DirPerm, jsonutil.FilePerm); err != nil {
+		return fmt.Errorf("failed to write currencies to a JSON file: %w", err)
+	}
+	return nil
 }

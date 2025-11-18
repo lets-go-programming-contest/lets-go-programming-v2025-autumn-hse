cddcdbb3d86a2482921d557e02cb2664065a61e3
OlesiaOl <olesia.olshv@gmail.com>
2025-11-18 12:01:15 +0300

    [TASK-3] jsonutil 1

--- olesia.olshevskaia/task-3/internal/jsonutil/jsonutil.go
+++ olesia.olshevskaia/task-3/internal/jsonutil/jsonutil.go
@@ -7,17 +7,32 @@ import (
 	"path/filepath"
 )
 
+const dirPer = 0o755
+
 func WriteOutput[T any](outputPath string, data T) error {
-	bytes, err := json.MarshalIndent(data, "", "  ")
+	bytes, err := json.Marshal(data)
 	if err != nil {
 		return fmt.Errorf("failed to marshal data to JSON: %w", err)
 	}
 
-	if err := os.MkdirAll(filepath.Dir(outputPath), 0o755); err != nil {
+	err = os.MkdirAll(filepath.Dir(outputPath), dirPer)
+	if err != nil {
 		return fmt.Errorf("failed to create output directory for %q: %w", outputPath, err)
 	}
 
-	if err := os.WriteFile(outputPath, bytes, 0o644); err != nil {
+	outputFile, err := os.Create(outputPath)
+	if err != nil {
+		return fmt.Errorf("failed to create output file %q: %w", outputPath, err)
+	}
+
+	defer func() {
+		if ferr := outputFile.Close(); ferr != nil {
+			panic(fmt.Errorf("failed to close file %q: %w", outputPath, ferr))
+		}
+	}()
+
+	_, err = outputFile.Write(bytes)
+	if err != nil {
 		return fmt.Errorf("failed to write to %q: %w", outputPath, err)
 	}
 

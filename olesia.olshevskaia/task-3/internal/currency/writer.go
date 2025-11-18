e3920cc78506db887de61fd4cad44e717d1b6502
OlesiaOl <olesia.olshv@gmail.com>
2025-11-18 11:51:56 +0300

    [TASK-3] o755 5

--- olesia.olshevskaia/task-3/internal/currency/writer.go
+++ olesia.olshevskaia/task-3/internal/currency/writer.go
@@ -1,15 +1,10 @@
 package currency
 
 import (
-	"fmt"
-
 	"github.com/Olesia.Ol/task-3/internal/jsonutil"
 	"github.com/Olesia.Ol/task-3/internal/model"
 )
 
 func WriteJSON(outputFile string, currencies []model.Currency) error {
-	if err := jsonutil.ParseJSON(outputFile, currencies); err != nil {
-		return fmt.Errorf("failed to write currencies to JSON file: %w", err)
-	}
-	return nil
+	return jsonutil.WriteOutput(outputFile, currencies)
 }
--- olesia.olshevskaia/task-3/internal/jsonutil/jsonutil.go
+++ olesia.olshevskaia/task-3/internal/jsonutil/jsonutil.go
@@ -7,34 +7,17 @@ import (
 	"path/filepath"
 )
 
-const (
-	DirPerm = 0o755
-)
-
-func ParseJSON[T any](outputPath string, data T) error {
-	bytes, err := json.Marshal(data)
+func WriteOutput[T any](outputPath string, data T) error {
+	bytes, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
 		return fmt.Errorf("failed to marshal data to JSON: %w", err)
 	}
 
-	err = os.MkdirAll(filepath.Dir(outputPath), DirPerm)
-	if err != nil {
+	if err := os.MkdirAll(filepath.Dir(outputPath), 0o755); err != nil {
 		return fmt.Errorf("failed to create output directory for %q: %w", outputPath, err)
 	}
 
-	outputFile, err := os.Create(outputPath)
-	if err != nil {
-		return fmt.Errorf("failed to create output file %q: %w", outputPath, err)
-	}
-
-	defer func() {
-		if ferr := outputFile.Close(); ferr != nil {
-			panic(fmt.Errorf("close file %q: %w", outputPath, err))
-		}
-	}()
-
-	_, err = outputFile.Write(bytes)
-	if err != nil {
+	if err := os.WriteFile(outputPath, bytes, 0o644); err != nil {
 		return fmt.Errorf("failed to write to %q: %w", outputPath, err)
 	}
 

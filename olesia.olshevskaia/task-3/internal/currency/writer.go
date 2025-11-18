20165421f8a786aeab241398a93e0ed1980b22f7
OlesiaOl <olesia.olshv@gmail.com>
2025-11-18 12:54:58 +0300

    [TASK-3] jsonutil 8

--- olesia.olshevskaia/task-3/internal/currency/writer.go
+++ olesia.olshevskaia/task-3/internal/currency/writer.go
@@ -1,10 +1,17 @@
 package currency
 
 import (
+	"os"
+
 	"github.com/Olesia.Ol/task-3/internal/jsonutil"
 	"github.com/Olesia.Ol/task-3/internal/model"
 )
 
+const (
+	dirPerm  = os.ModePerm
+	filePerm = 0o644
+)
+
 func WriteJSON(outputFile string, currencies []model.Currency) error {
-	return jsonutil.WriteOutput(outputFile, currencies, 0o755, 0o600)
+	return jsonutil.ParseJSON(outputFile, currencies, dirPerm, filePerm)
 }

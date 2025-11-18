35264167b1a16439030da5b496e8f664c8930ead
OlesiaOl <olesia.olshv@gmail.com>
2025-11-18 10:31:34 +0300

    [TASK-3] whitespace ;)

--- olesia.olshevskaia/task-3/internal/currency/writer.go
+++ olesia.olshevskaia/task-3/internal/currency/writer.go
@@ -11,5 +11,6 @@ func WriteJSON(outputFile string, currencies []model.Currency) error {
 	if err := jsonutil.ParseJSON(outputFile, currencies, jsonutil.DirPerm, jsonutil.FilePerm); err != nil {
 		return fmt.Errorf("failed to write currencies to a JSON file: %w", err)
 	}
+
 	return nil
 }

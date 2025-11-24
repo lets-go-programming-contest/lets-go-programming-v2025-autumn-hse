31c79e14e89bbf69efbbb99ede4420afc2dcc693
Nevermind0911 <repinarti@gmail.com>
2025-11-24 16:35:53 +0300

    [TASK-2-2] uberite linter

--- artemiy.repin/task-2-2/cmd/service/main.go
+++ artemiy.repin/task-2-2/cmd/service/main.go
@@ -17,14 +17,32 @@ func main() {
 	}()
 
 	input := bufio.NewReader(os.Stdin)
+
+	minHeap, heapCount, preferredRank, ok := prepareHeap(input)
+	if !ok {
+		return
+	}
+
+	if !popUntilRank(minHeap, heapCount, preferredRank) {
+		return
+	}
+
+	printTop(minHeap)
+}
+
+func prepareHeap(input *bufio.Reader) (*myheap.MinHeap, int, int, bool) {
 	var heapCount int
 
 	if _, err := fmt.Fscan(input, &heapCount); err != nil {
-		fmt.Println("cannot read heap count"); return
+		fmt.Println("cannot read heap count")
+
+		return nil, 0, 0, false
 	}
 
 	if heapCount <= 0 {
-		fmt.Println("heap count must be > 0"); return
+		fmt.Println("heap count must be > 0")
+
+		return nil, 0, 0, false
 	}
 
 	minHeap := &myheap.MinHeap{}
@@ -34,38 +52,67 @@ func main() {
 		var preferenceScore int
 
 		if _, err := fmt.Fscan(input, &preferenceScore); err != nil {
-			fmt.Println("cannot read preference score"); return
+			fmt.Println("cannot read preference score")
+
+			return nil, 0, 0, false
 		}
+
 		stdheap.Push(minHeap, preferenceScore)
 	}
 
 	var preferredRank int
 
 	if _, err := fmt.Fscan(input, &preferredRank); err != nil {
-		fmt.Println("cannot read preferred rank"); return
+		fmt.Println("cannot read preferred rank")
+
+		return nil, 0, 0, false
 	}
 
 	if preferredRank < 1 || preferredRank > heapCount {
-		fmt.Println("preferred rank is out of range"); return
+		fmt.Println("preferred rank is out of range")
+
+		return nil, 0, 0, false
 	}
 
-	for range heapCount - preferredRank {
+	return minHeap, heapCount, preferredRank, true
+}
 
+func popUntilRank(minHeap *myheap.MinHeap, heapCount, preferredRank int) bool {
+	popsCount := heapCount - preferredRank
+
+	for range popsCount {
 		if minHeap.Len() == 0 {
-			fmt.Println("heap is empty, cannot pop"); return
+			fmt.Println("heap is empty, cannot pop")
+
+			return false
 		}
 
 		stdheap.Pop(minHeap)
 	}
 
+	return true
+}
+
+func printTop(minHeap *myheap.MinHeap) {
 	if minHeap.Len() == 0 {
-		fmt.Println("heap is empty before final pop"); return
+		fmt.Println("heap is empty before final pop")
+
+		return
 	}
 
 	value := stdheap.Pop(minHeap)
-	intValue, ok := value.(int)
-	if !ok {
-		fmt.Println("heap value is not int"); return
+	if value == nil {
+		fmt.Println("got nil from heap")
+
+		return
 	}
-	fmt.Println(intValue)
+
+	val, ok := value.(int)
+	if !ok {
+		fmt.Println("heap value is not int")
+
+		return
+	}
+
+	fmt.Println(val)
 }

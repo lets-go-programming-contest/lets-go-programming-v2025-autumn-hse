225db5f80bc133f7c8a13b90031b3e6a6da1e734
Ekaterina-101 <mnm54637@gmail.com>
2025-11-26 16:33:03 +0300

    [TASK-5] handlers

--- ekaterina.kuznetsova/task-5/pkg/handlers/handlers.go
+++ ekaterina.kuznetsova/task-5/pkg/handlers/handlers.go
@@ -1,51 +1,68 @@
 package handlers
 
 import (
+	"context"
 	"errors"
 	"strings"
 )
 
-func PrefixDecoratorFunc(input chan string, output chan string) error {
+func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
 	for val := range input {
-		if strings.Contains(val, "no decorator") {
-			return errors.New("can't be decorated")
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			if strings.Contains(val, "no decorator") {
+				return errors.New("can't be decorated")
+			}
+			if !strings.HasPrefix(val, "decorated: ") {
+				val = "decorated: " + val
+			}
+			output <- val
 		}
-
-		if !strings.HasPrefix(val, "decorated: ") {
-			val = "decorated: " + val
-		}
-
-		output <- val
 	}
-
 	return nil
 }
 
-func SeparatorFunc(input chan string, outputs []chan string) error {
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	if len(outputs) == 0 {
+		return errors.New("no output channels")
+	}
 	index := 0
 	for val := range input {
-		outputs[index] <- val
-		index = (index + 1) % len(outputs)
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			outputs[index] <- val
+			index = (index + 1) % len(outputs)
+		}
 	}
-
 	return nil
 }
 
-func MultiplexerFunc(inputs []chan string, output chan string) error {
+func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	if len(inputs) == 0 {
+		return errors.New("no input channels")
+	}
 	for {
 		allClosed := true
-
 		for _, ch := range inputs {
-			val, ok := <-ch
-			if ok {
-				allClosed = false
-				if strings.Contains(val, "no multiplexer") {
-					continue
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case val, ok := <-ch:
+				if ok {
+					allClosed = false
+					if strings.Contains(val, "no multiplexer") {
+						continue
+					}
+					output <- val
 				}
-				output <- val
+			default:
+				allClosed = false
 			}
 		}
-
 		if allClosed {
 			return nil
 		}

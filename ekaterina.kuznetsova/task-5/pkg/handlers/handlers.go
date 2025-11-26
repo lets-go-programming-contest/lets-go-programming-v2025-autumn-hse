4cda60b757f73d4c97cb95fae4342dae15ecec12
Ekaterina-101 <mnm54637@gmail.com>
2025-11-26 22:06:02 +0300

    [TASK-5] fix handlers

--- ekaterina.kuznetsova/task-5/pkg/handlers/handlers.go
+++ ekaterina.kuznetsova/task-5/pkg/handlers/handlers.go
@@ -8,70 +8,49 @@ import (
 
 func PrefixDecoratorFunc(ctx context.Context, in, out chan string) error {
 	for {
-		select {
-		case <-ctx.Done():
+		if ctx.Err() != nil {
 			return nil
-		case val, ok := <-in:
-			if !ok {
-				return nil
-			}
-			if strings.Contains(val, "no decorator") {
-				return errors.New("can't be decorated")
-			}
-			if !strings.HasPrefix(val, "decorated: ") {
-				val = "decorated: " + val
-			}
-			select {
-			case <-ctx.Done():
-				return nil
-			case out <- val:
-			}
 		}
+		val, ok := <-in
+		if !ok {
+			return nil
+		}
+		if strings.Contains(val, "no decorator") {
+			return errors.New("can't be decorated")
+		}
+		if !strings.HasPrefix(val, "decorated: ") {
+			val = "decorated: " + val
+		}
+		out <- val
 	}
 }
 
 func SeparatorFunc(ctx context.Context, in chan string, outs []chan string) error {
 	i := 0
 	for {
-		select {
-		case <-ctx.Done():
+		if ctx.Err() != nil {
 			return nil
-		case val, ok := <-in:
-			if !ok {
-				return nil
-			}
-			select {
-			case <-ctx.Done():
-				return nil
-			case outs[i] <- val:
-			}
-			i = (i + 1) % len(outs)
 		}
+		val, ok := <-in
+		if !ok {
+			return nil
+		}
+		outs[i] <- val
+		i = (i + 1) % len(outs)
 	}
 }
 
 func MultiplexerFunc(ctx context.Context, ins []chan string, out chan string) error {
 	for {
-		select {
-		case <-ctx.Done():
+		if ctx.Err() != nil {
 			return nil
-		default:
 		}
 		for _, ch := range ins {
-			select {
-			case <-ctx.Done():
-				return nil
-			case val, ok := <-ch:
-				if !ok || strings.Contains(val, "no multiplexer") {
-					continue
-				}
-				select {
-				case <-ctx.Done():
-					return nil
-				case out <- val:
-				}
-			default:
+			val, ok := <-ch
+			if !ok || strings.Contains(val, "no multiplexer") {
+				continue
 			}
+			out <- val
 		}
 	}
 }

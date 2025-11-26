2981a6ed46c78bff3153a8c093713570888dcdc8
Ekaterina-101 <mnm54637@gmail.com>
2025-11-26 22:16:42 +0300

    [TASK-5] fix handlers 2

--- ekaterina.kuznetsova/task-5/pkg/handlers/handlers.go
+++ ekaterina.kuznetsova/task-5/pkg/handlers/handlers.go
@@ -8,49 +8,62 @@ import (
 
 func PrefixDecoratorFunc(ctx context.Context, in, out chan string) error {
 	for {
-		if ctx.Err() != nil {
+		select {
+		case <-ctx.Done():
 			return nil
+		case val, ok := <-in:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(val, "no decorator") {
+				return errors.New("can’t be decorated")
+			}
+
+			if !strings.HasPrefix(val, "decorated: ") {
+				val = "decorated: " + val
+			}
+
+			out <- val
 		}
-		val, ok := <-in
-		if !ok {
-			return nil
-		}
-		if strings.Contains(val, "no decorator") {
-			return errors.New("can't be decorated")
-		}
-		if !strings.HasPrefix(val, "decorated: ") {
-			val = "decorated: " + val
-		}
-		out <- val
 	}
 }
 
 func SeparatorFunc(ctx context.Context, in chan string, outs []chan string) error {
 	i := 0
+
 	for {
-		if ctx.Err() != nil {
+		select {
+		case <-ctx.Done():
 			return nil
+		case val, ok := <-in:
+			if !ok {
+				return nil
+			}
+
+			outs[i] <- val
+			i = (i + 1) % len(outs)
 		}
-		val, ok := <-in
-		if !ok {
-			return nil
-		}
-		outs[i] <- val
-		i = (i + 1) % len(outs)
 	}
 }
 
 func MultiplexerFunc(ctx context.Context, ins []chan string, out chan string) error {
 	for {
-		if ctx.Err() != nil {
-			return nil
-		}
 		for _, ch := range ins {
-			val, ok := <-ch
-			if !ok || strings.Contains(val, "no multiplexer") {
-				continue
+			select {
+			case <-ctx.Done():
+				return nil
+			case val, ok := <-ch:
+				if !ok {
+					continue
+				}
+				if strings.Contains(val, "no multiplexer") {
+					continue
+				}
+
+				out <- val
+			default:
 			}
-			out <- val
 		}
 	}
 }

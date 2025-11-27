cbbed3728d86ed21682854f77ca7b52bd8cabb5e
kef1rch1k <kristinalotonina@yandex.ru>
2025-11-27 17:50:01 +0300

    [TASK-3] (#59)
    * [TASK-3] up task-3
    
    * [TASK-3] add windows-1251
    
    * [TASK-3] fix linter
    
    * [TASK-3] fix fmt
    
    * [TASK-3] fix file close
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] names i
    
    * [TASK-3] errors %w
    
    * [TASK-3] fmt import
    
    * [TASK-3] package name & errors
    
    * [TASK-3] error wrapped
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] rename package
    
    * [TASK-3] build
    
    * [TASK-3] lint
    
    * [TASK-3] one struct
    
    * [TASK-3] single struct and new method
    
    * [TASK-3] style
    
    * [TASK-3] short name
    
    * [TASK-3] lint
    
    * [TASK-3] lint
    
    * [TASK-3] new package json
    
    * [TASK-3] build
    
    * [TASK-3] check return value
    
    * [TASK-3] sort logic in main
    
    * [TASK-3] universal parser
    
    * [TASK-3] default flag
    
    * [TASK-3] without new variable

---
+++ kristina.lotonina/task-3/internal/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("reading config file: %w", err)
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, fmt.Errorf("unmarshalling yaml: %w", err)
+	}
+
+	return &cfg, nil
+}

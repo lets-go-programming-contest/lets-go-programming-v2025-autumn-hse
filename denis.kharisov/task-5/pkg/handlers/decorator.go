4a2cc864563bc9c711a056f88feecb32e299f532
denisK-H <fjk5940@gmail.com>
2025-12-07 21:59:24 +0300

    [TASK-5] fix errCantBeDecorated

--- denis.kharisov/task-5/pkg/handlers/decorator.go
+++ denis.kharisov/task-5/pkg/handlers/decorator.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-var errCantBeDecorated = errors.New("can`t be decorated")
+var errCantBeDecorated = errors.New("can't be decorated")
 
 const (
 	subStrNoDecorator = "no decorator"

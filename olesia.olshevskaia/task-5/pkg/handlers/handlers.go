a0cf7903d1d86e75cae5ae57cec1a1bedf4edeae
OlesiaOl <olesia.olshv@gmail.com>
2025-12-08 23:24:35 +0300

    [TASK-5]  (#75)
    * [TASK-5] Initial commit
    
    * [TASK-5] sync@v0.1.0
    
    * [TASK-5] create channels automatically
    
    * [TASK-5] MultiplexerFunc
    
    * [TASK-5] MultiplexerFunc 2
    
    * [TASK-5] MultiplexerFunc 3
    
    * [TASK-5] MultiplexerFunc 4
    
    * [TASK-5] run
    
    * [TASK-5] select delete
    
    * [TASK-5] run 2
    
    * [TASK-5] lint 1
    
    * [TASK-5] lint 2
    
    * [TASK-5] lint 3
    
    * [TASK-5] a lot of init
    
    * [TASK-5] RWMutex
    
    * [TASK-5] select
    
    * [TASK-5] select come back
    
    * [TASK-5] select 1
    
    * [TASK-5] lint 1
    
    * [TASK-5] lint 2

---
+++ olesia.olshevskaia/task-5/pkg/handlers/handlers.go
@@ -0,0 +1,90 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+)
+
+var errCantBeDecorated = errors.New("can't be decorated")
+
+const (
+	skipDecorator   = "no decorator"
+	decoratedPrefix = "decorated: "
+	skipMultiplexer = "no multiplexer"
+)
+
+func PrefixDecoratorFunc(ctx context.Context, input, output chan string) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+		}
+
+		var data string
+
+		select {
+		case <-ctx.Done():
+			return nil
+		case v, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			data = v
+		}
+
+		if strings.Contains(data, skipDecorator) {
+			return errCantBeDecorated
+		}
+
+		if !strings.HasPrefix(data, decoratedPrefix) {
+			data = decoratedPrefix + data
+		}
+
+		select {
+		case <-ctx.Done():
+			return nil
+		case output <- data:
+		}
+	}
+}
+
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	idn := 0
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+			outputs[idn] <- data
+			idn = (idn + 1) % len(outputs)
+		}
+	}
+}
+
+func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	for {
+		for _, ch := range inputs {
+			select {
+			case <-ctx.Done():
+				return nil
+			case value, ok := <-ch:
+				if !ok {
+					continue
+				}
+
+				if strings.Contains(value, skipMultiplexer) {
+					continue
+				}
+				output <- value
+			default:
+			}
+		}
+	}
+}

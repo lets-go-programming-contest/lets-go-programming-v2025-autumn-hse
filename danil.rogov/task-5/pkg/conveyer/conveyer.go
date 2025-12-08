ce49bb8d495e7e85cde8c565721e55818965989b
Tapochek2894 <dvrogov06@yandex.ru>
2025-12-08 23:28:15 +0300

    [TASK-5] (#76)
    * [TASK-5] initial
    
    * [TASK-5] change handlers
    
    * [TASK-5] change registration
    
    * [TASK-5] change error
    
    * [TASK-5] change run func
    
    * [TASK-5] change return value of handlers
    
    * [TASK-5] change return value of handlers v2
    
    * [TASK-5] change files and fix lint
    
    * [TASK-5] fix lint
    
    * [TASK-5] lint changes
    
    * [TASK-5] last change
    
    * [TASK-5] change constant and mutex

---
+++ danil.rogov/task-5/pkg/conveyer/conveyer.go
@@ -0,0 +1,129 @@
+package conveyer
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
+
+var (
+	ErrChanNotFound           = errors.New("chan not found")
+	ErrChanIsFull             = errors.New("chan is full")
+	ErrUnknownTask            = errors.New("unknown task type")
+	ErrInvalidDecoratorType   = errors.New("invalid decorator function type")
+	ErrInvalidMultiplexerType = errors.New("invalid multiplexer function type")
+	ErrInvalidSeparatorType   = errors.New("invalid separator function type")
+)
+
+const chanIsClosedAndEmpty = "undefined"
+
+type Conveyer struct {
+	channels map[string]chan string
+	size     int
+	tasks    []task
+	mutex    sync.RWMutex
+}
+
+type task struct {
+	name        string
+	function    any
+	inputChans  []string
+	outputChans []string
+}
+
+func New(size int) *Conveyer {
+	return &Conveyer{
+		channels: make(map[string]chan string),
+		size:     size,
+		tasks:    make([]task, 0),
+		mutex:    sync.RWMutex{},
+	}
+}
+
+func (conveyer *Conveyer) findChan(name string) (chan string, bool) {
+	conveyer.mutex.RLock()
+	defer conveyer.mutex.RUnlock()
+
+	channel, found := conveyer.channels[name]
+
+	return channel, found
+}
+
+func (conveyer *Conveyer) ensureChan(name string) chan string {
+	conveyer.mutex.Lock()
+	defer conveyer.mutex.Unlock()
+
+	channel, found := conveyer.channels[name]
+	if !found {
+		channel = make(chan string, conveyer.size)
+		conveyer.channels[name] = channel
+	}
+
+	return channel
+}
+
+func (conveyer *Conveyer) Run(ctx context.Context) error {
+	defer func() {
+		conveyer.mutex.Lock()
+		for _, ch := range conveyer.channels {
+			close(ch)
+		}
+		conveyer.mutex.Unlock()
+	}()
+
+	errGroup, _ := errgroup.WithContext(ctx)
+
+	for _, task := range conveyer.tasks {
+		errGroup.Go(func() error {
+			switch task.name {
+			case "decorator":
+				return conveyer.executeDecorator(ctx, task)
+			case "multiplexer":
+				return conveyer.executeMultiplexer(ctx, task)
+			case "separator":
+				return conveyer.executeSeparator(ctx, task)
+			default:
+				return ErrUnknownTask
+			}
+		})
+	}
+
+	err := errGroup.Wait()
+	if err != nil {
+		return fmt.Errorf("error while executing tasks: %w", err)
+	}
+
+	return nil
+}
+
+func (conveyer *Conveyer) Send(name, data string) error {
+	channel, found := conveyer.findChan(name)
+	if !found {
+		return ErrChanNotFound
+	}
+
+	if len(channel) == cap(channel) {
+		return ErrChanIsFull
+	}
+
+	channel <- data
+
+	return nil
+}
+
+func (conveyer *Conveyer) Recv(name string) (string, error) {
+	channel, found := conveyer.findChan(name)
+	if !found {
+		return chanIsClosedAndEmpty, ErrChanNotFound
+	}
+
+	val, ok := <-channel
+	if !ok {
+		return chanIsClosedAndEmpty, nil
+	}
+
+	return val, nil
+}

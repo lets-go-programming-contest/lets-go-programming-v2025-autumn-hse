c94ba6138c16c0c6a2486ae468e687225107f2f0
Vladislav <mainmail30@bk.ru>
2025-12-11 13:30:12 -0500

    [TASK-6] Initial commit : Attempt 7

--- vladislav.peleev/task-6/internal/db/db_test.go
+++ vladislav.peleev/task-6/internal/db/db_test.go
@@ -124,3 +124,33 @@ func TestGetUniqueNames_ScanError(t *testing.T) {
 	require.Contains(t, err.Error(), "rows scanning:")
 	require.NoError(t, mock.ExpectationsWereMet())
 }
+
+func TestGetNames_RowsErr(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"name"}).AddRow("Alice").CloseError(errors.New("close error"))
+	mock.ExpectQuery("SELECT name FROM users").WillReturnRows(rows)
+
+	s := New(db)
+	_, err = s.GetNames()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "rows error:")
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestGetUniqueNames_RowsErr(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"name"}).AddRow("Bob").CloseError(errors.New("close error"))
+	mock.ExpectQuery("SELECT DISTINCT name FROM users").WillReturnRows(rows)
+
+	s := New(db)
+	_, err = s.GetUniqueNames()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "rows error:")
+	require.NoError(t, mock.ExpectationsWereMet())
+}

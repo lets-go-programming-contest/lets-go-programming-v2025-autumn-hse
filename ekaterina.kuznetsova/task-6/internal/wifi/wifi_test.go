40cf39cddc520da1e5fcc44426b57afc7f52062d
Ekaterina-101 <mnm54637@gmail.com>
2025-12-12 14:12:11 +0300

    [TASK-6] test 100% 28

--- ekaterina.kuznetsova/task-6/internal/wifi/wifi_test.go
+++ ekaterina.kuznetsova/task-6/internal/wifi/wifi_test.go
@@ -7,12 +7,22 @@ import (
 	"testing"
 
 	myWifi "github.com/Ekaterina-101/task-6/internal/wifi"
-	
+
 	"github.com/mdlayher/wifi"
 	"github.com/stretchr/testify/require"
 )
 
 //go:generate mockery --all --testonly --quiet --outpkg wifi_test --output .
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	mockWifi := &wifiMocks.MockWiFi{}
+	wifiService := myWifi.New(mockWifi)
+
+	require.NotNil(t, wifiService, "WiFiService should not be nil")
+	require.Equal(t, mockWifi, wifiService.WiFi, "WiFi field should be set correctly")
+}
 
 type rowTestSysInfo struct {
 	addrs       []string
@@ -136,16 +146,16 @@ func mockInterfacesByNames(names []string) []*wifi.Interface {
 
 	for i, name := range names {
 		iface := &wifi.Interface{
-			Index:     i + 1,
-			Name:      name,
-			PHY:       1,
-			Device:    1,
-			Type:      wifi.InterfaceTypeAPVLAN,
-			Frequency: 0,
+			Index:        i + 1,
+			Name:         name,
+			HardwareAddr: parseMAC("00:00:00:00:00:00"),
+			PHY:          1,
+			Device:       1,
+			Type:         wifi.InterfaceTypeAPVLAN,
+			Frequency:    0,
 		}
 		interfaces = append(interfaces, iface)
 	}
 
 	return interfaces
 }
-

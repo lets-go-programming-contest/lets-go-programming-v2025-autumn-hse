f4ec15c969310a3198566cc2d77cb5dca14d3452
OlesiaOl <olesia.olshv@gmail.com>
2025-12-12 22:25:45 +0300

    [TASK-6]  (#84)
    * [TASK-6] init commit
    
    * [TASK-6] db 100 per sent
    
    * [TASK-6] version
    
    * [TASK-6] dependencies
    
    * [TASK-6] delete
    
    * [TASK-6] lint 1
    
    * [TASK-6] lint 2
    
    * [TASK-6] ErrorContains testing
    
    * [TASK-6] ErrorContains all tests
    
    * [TASK-6] ErrorContains +wifi tests
    
    * [TASK-6] require
    
    * [TASK-6] go generated
    
    * [TASK-6] go generated 2
    
    * [TASK-6] go generated 3
    
    * [TASK-6] wifi.NewWiFiHandle
    
    * [TASK-6] generate
    
    * [TASK-6] strings one type
    
    * [TASK-6] strings one type 2
    
    * [TASK-6] strings one type db

---
+++ olesia.olshevskaia/task-6/internal/db/db_test.go
@@ -0,0 +1,212 @@
+package db_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/OlesiaOl/task-6/internal/db"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	errLostConnection = errors.New("connection lost")
+	errRowBroken      = errors.New("row broken")
+	errRow            = errors.New("some rows error")
+)
+
+const (
+	nameTom   = "Tom"
+	nameJerry = "Jerry"
+)
+
+func TestGetNamesOK(t *testing.T) {
+	t.Parallel()
+
+	sqlDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer sqlDB.Close()
+
+	service := db.New(sqlDB)
+
+	rows := sqlmock.NewRows([]string{"name"}).
+		AddRow(nameTom).
+		AddRow(nameJerry)
+
+	mock.ExpectQuery("SELECT name FROM users").
+		WillReturnRows(rows)
+
+	names, err := service.GetNames()
+
+	require.NoError(t, err)
+	require.Equal(t, []string{nameTom, nameJerry}, names)
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestGetNamesQueryError(t *testing.T) {
+	t.Parallel()
+
+	sqlDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer sqlDB.Close()
+
+	service := db.New(sqlDB)
+
+	mock.ExpectQuery("SELECT name FROM users").
+		WillReturnError(errLostConnection)
+
+	_, err = service.GetNames()
+
+	require.ErrorContains(t, err, "db query")
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestGetNamesRowsError(t *testing.T) {
+	t.Parallel()
+
+	sqlDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer sqlDB.Close()
+
+	service := db.New(sqlDB)
+
+	rows := sqlmock.NewRows([]string{"name"}).
+		AddRow(nameTom).
+		AddRow(nameJerry).
+		RowError(1, errRowBroken)
+
+	mock.ExpectQuery("SELECT name FROM users").
+		WillReturnRows(rows)
+
+	_, err = service.GetNames()
+
+	require.ErrorContains(t, err, "rows error")
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestGetNamesScanError(t *testing.T) {
+	t.Parallel()
+
+	sqlDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer sqlDB.Close()
+
+	service := db.New(sqlDB)
+
+	rows := sqlmock.NewRows([]string{"name"}).
+		AddRow(nil)
+
+	mock.ExpectQuery("SELECT name FROM users").
+		WillReturnRows(rows)
+
+	_, err = service.GetNames()
+
+	require.ErrorContains(t, err, "rows scanning")
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestGetUniqueNamesRowsErr(t *testing.T) {
+	t.Parallel()
+
+	sqlDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer sqlDB.Close()
+
+	service := db.New(sqlDB)
+
+	rows := sqlmock.NewRows([]string{"name"}).
+		AddRow(nameTom).
+		RowError(0, errRow)
+
+	mock.ExpectQuery("SELECT DISTINCT name FROM users").
+		WillReturnRows(rows)
+
+	_, err = service.GetUniqueNames()
+
+	require.ErrorContains(t, err, "rows error")
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestGetUniqueNamesOK(t *testing.T) {
+	t.Parallel()
+
+	sqlDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer sqlDB.Close()
+
+	service := db.New(sqlDB)
+
+	rows := sqlmock.NewRows([]string{"name"}).
+		AddRow(nameTom).
+		AddRow(nameJerry)
+
+	mock.ExpectQuery("SELECT DISTINCT name FROM users").
+		WillReturnRows(rows)
+
+	names, err := service.GetUniqueNames()
+
+	require.NoError(t, err)
+	require.Equal(t, []string{nameTom, nameJerry}, names)
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestGetUniqueNamesQueryError(t *testing.T) {
+	t.Parallel()
+
+	sqlDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer sqlDB.Close()
+
+	service := db.New(sqlDB)
+
+	mock.ExpectQuery("SELECT DISTINCT name FROM users").
+		WillReturnError(errLostConnection)
+
+	_, err = service.GetUniqueNames()
+
+	require.ErrorContains(t, err, "db query")
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestGetUniqueNamesScanError(t *testing.T) {
+	t.Parallel()
+
+	sqlDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer sqlDB.Close()
+
+	service := db.New(sqlDB)
+
+	rows := sqlmock.NewRows([]string{"name"}).
+		AddRow(nil)
+
+	mock.ExpectQuery("SELECT DISTINCT name FROM users").
+		WillReturnRows(rows)
+
+	_, err = service.GetUniqueNames()
+
+	require.ErrorContains(t, err, "rows scanning")
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestGetUniqueNamesRowsError(t *testing.T) {
+	t.Parallel()
+
+	sqlDB, mock, err := sqlmock.New()
+	require.NoError(t, err)
+	defer sqlDB.Close()
+
+	service := db.New(sqlDB)
+
+	rows := sqlmock.NewRows([]string{"name"}).
+		AddRow(nameJerry).
+		RowError(0, errRow)
+
+	mock.ExpectQuery("SELECT DISTINCT name FROM users").
+		WillReturnRows(rows)
+
+	_, err = service.GetUniqueNames()
+
+	require.ErrorContains(t, err, "rows error")
+	require.NoError(t, mock.ExpectationsWereMet())
+}
---
+++ olesia.olshevskaia/task-6/internal/wifi/wifi_test.go
@@ -0,0 +1,162 @@
+//go:generate mockery --name=WiFiHandle --testonly --quiet --outpkg wifi_test --output .
+package wifi_test
+
+import (
+	"errors"
+	"net"
+	"testing"
+
+	"github.com/OlesiaOl/task-6/internal/wifi"
+
+	wifipkg "github.com/mdlayher/wifi"
+	"github.com/stretchr/testify/require"
+)
+
+var errTest = errors.New("test error")
+
+const (
+	interfaceName1 = "nevergivup"
+	interfaceName2 = "Naf-Naf"
+	interfaceName3 = "Nuf-Nuf"
+	interfaceName4 = "Nif-Nif"
+)
+
+func TestWiFiGetNamesOneNoError(t *testing.T) {
+	t.Parallel()
+
+	wifiHandler := NewWiFiHandle(t)
+	service := wifi.New(wifiHandler)
+	expectedResult := []string{interfaceName1}
+
+	wifiHandler.On("Interfaces").Return([]*wifipkg.Interface{
+		{
+			Name: interfaceName1,
+		},
+	}, nil)
+
+	realNames, err := service.GetNames()
+
+	require.NoError(t, err)
+	require.Equal(t, expectedResult, realNames)
+
+	wifiHandler.AssertExpectations(t)
+}
+
+func TestWiFiGetNamesMultipleNoError(t *testing.T) {
+	t.Parallel()
+
+	wifiHandler := NewWiFiHandle(t)
+	service := wifi.New(wifiHandler)
+	expectedResult := []string{interfaceName2, interfaceName3, interfaceName4}
+
+	wifiHandler.On("Interfaces").Return([]*wifipkg.Interface{
+		{
+			Name: interfaceName2,
+		},
+		{
+			Name: interfaceName3,
+		},
+		{
+			Name: interfaceName4,
+		},
+	}, nil)
+
+	realNames, err := service.GetNames()
+
+	require.NoError(t, err)
+	require.Equal(t, expectedResult, realNames)
+
+	wifiHandler.AssertExpectations(t)
+}
+
+func TestWiFIGetNamesWithError(t *testing.T) {
+	t.Parallel()
+
+	wifiHandler := NewWiFiHandle(t)
+	service := wifi.New(wifiHandler)
+
+	wifiHandler.On("Interfaces").Return(nil, errTest)
+
+	_, err := service.GetNames()
+
+	require.ErrorContains(t, err, "getting interfaces")
+	require.ErrorIs(t, err, errTest)
+
+	wifiHandler.AssertExpectations(t)
+}
+
+func TestWiFiGetAddressesOneNoError(t *testing.T) {
+	t.Parallel()
+
+	mockHandler := NewWiFiHandle(t)
+	service := wifi.New(mockHandler)
+
+	mac1, _ := net.ParseMAC("00:11:22:33:44:55")
+	expectedResult := []net.HardwareAddr{mac1}
+
+	mockHandler.On("Interfaces").Return([]*wifipkg.Interface{
+		{
+			HardwareAddr: mac1,
+		},
+	}, nil)
+
+	actualAddresses, err := service.GetAddresses()
+
+	require.NoError(t, err)
+	require.Equal(t, expectedResult, actualAddresses)
+	require.Len(t, actualAddresses, 1)
+
+	mockHandler.AssertExpectations(t)
+}
+
+func TestWiFiGetAddressesMultipleNoError(t *testing.T) {
+	t.Parallel()
+
+	mockHandler := NewWiFiHandle(t)
+	service := wifi.New(mockHandler)
+
+	mac1, _ := net.ParseMAC("00:11:22:33:44:55")
+	mac2, _ := net.ParseMAC("AA:BB:CC:DD:EE:FF")
+	mac3, _ := net.ParseMAC("11:22:33:44:55:66")
+
+	expectedResult := []net.HardwareAddr{mac1, mac2, mac3}
+
+	mockHandler.On("Interfaces").Return([]*wifipkg.Interface{
+		{
+			Name:         interfaceName2,
+			HardwareAddr: mac1,
+		},
+		{
+			Name:         interfaceName3,
+			HardwareAddr: mac2,
+		},
+		{
+			Name:         interfaceName4,
+			HardwareAddr: mac3,
+		},
+	}, nil)
+
+	actualAddresses, err := service.GetAddresses()
+
+	require.NoError(t, err)
+	require.Equal(t, expectedResult, actualAddresses)
+	require.Len(t, actualAddresses, 3)
+
+	mockHandler.AssertExpectations(t)
+}
+
+func TestWiFiGetAddressesWithError(t *testing.T) {
+	t.Parallel()
+
+	mockHandler := NewWiFiHandle(t)
+	service := wifi.New(mockHandler)
+
+	mockHandler.On("Interfaces").Return(nil, errTest)
+
+	_, err := service.GetAddresses()
+
+	require.ErrorContains(t, err, "getting interfaces")
+	require.ErrorIs(t, err, errTest)
+
+	mockHandler.AssertExpectations(t)
+}

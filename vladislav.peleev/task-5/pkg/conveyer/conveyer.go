f8d76ecd1a230f9631d5c47820b8db2b12eb1c0c
VlasfimosY <vlasfimosy@gmail.com>
2025-12-12 23:15:35 +0300

    [TASK-5] (#82)
    * [TASK-5] Initial commit : Attempt 0
    
    * [TASK-5] Initial commit : Attempt 1
    
    * [TASK-5] Initial commit : Attempt 2
    
    * [TASK-5] Initial commit : Attempt 3
    
    * [TASK-5] Initial commit : Attempt 4
    
    * [TASK-5] Initial commit : Attempt 5
    
    * [TASK-5] Initial commit : Attempt 6
    
    * [TASK-5] Initial commit : Attempt 7
    
    * [TASK-5] Initial commit : Attempt 8
    
    * [TASK-5] Initial commit : Attempt 9
    
    * [TASK-5] Initial commit : Attempt 10
    
    * [TASK-5] Initial commit : Attempt 11
    
    * [TASK-5] Initial commit : Attempt 12
    
    * [TASK-5] Initial commit : Attempt 13
    
    * [TASK-5] Initial commit : Attempt 14
    
    * [TASK-5] Initial commit : Attempt 15
    
    * [TASK-5] Initial commit : Attempt 16
    
    * [TASK-5] Initial commit : Attempt 17
    
    * [TASK-5] Fix №1 : Attempt 0
    
    * [TASK-5] Fix №1 : Attempt 1
    
    * [TASK-5] Fix №2 : Attempt 0
    
    * [TASK-5] Fix №2 : Attempt 2
    
    * [TASK-5] Fix №2 : Attempt 3
    
    ---------
    
    Co-authored-by: Vladislav <mainmail30@bk.ru>

---
+++ vladislav.peleev/task-5/pkg/conveyer/conveyer.go
@@ -0,0 +1,216 @@
+package conveyer
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
+
+const recvValueOnClosedChannel = "undefined"
+
+var (
+	errCannotRegisterAfterRun = errors.New("cannot register after Run")
+	errAlreadyStarted         = errors.New("already started")
+	errChanNotFound           = errors.New("chan not found")
+)
+
+type Conveyer interface {
+	RegisterDecorator(
+		handlerFunc func(context.Context, chan string, chan string) error,
+		input, output string,
+	) error
+
+	RegisterMultiplexer(
+		handlerFunc func(context.Context, []chan string, chan string) error,
+		inputs []string,
+		output string,
+	) error
+
+	RegisterSeparator(
+		handlerFunc func(context.Context, chan string, []chan string) error,
+		input string,
+		outputs []string,
+	) error
+
+	Run(ctx context.Context) error
+	Send(input, data string) error
+	Recv(output string) (string, error)
+}
+
+type conveyerImpl struct {
+	mu       sync.RWMutex
+	channels map[string]chan string
+	handlers []func(context.Context) error
+	started  bool
+	bufSize  int
+}
+
+func New(size int) *conveyerImpl {
+	return &conveyerImpl{
+		mu:       sync.RWMutex{},
+		channels: make(map[string]chan string),
+		handlers: nil,
+		started:  false,
+		bufSize:  size,
+	}
+}
+
+func (c *conveyerImpl) ensureChannel(name string) chan string {
+	if channel, exists := c.channels[name]; exists {
+		return channel
+	}
+
+	channel := make(chan string, c.bufSize)
+	c.channels[name] = channel
+
+	return channel
+}
+
+func (c *conveyerImpl) RegisterDecorator(
+	handlerFunc func(context.Context, chan string, chan string) error,
+	input, output string,
+) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.started {
+		return errCannotRegisterAfterRun
+	}
+
+	inChannel := c.ensureChannel(input)
+	outChannel := c.ensureChannel(output)
+
+	c.handlers = append(c.handlers, func(ctx context.Context) error {
+		return handlerFunc(ctx, inChannel, outChannel)
+	})
+
+	return nil
+}
+
+func (c *conveyerImpl) RegisterSeparator(
+	handlerFunc func(context.Context, chan string, []chan string) error,
+	input string, outputs []string,
+) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.started {
+		return errCannotRegisterAfterRun
+	}
+
+	inChannel := c.ensureChannel(input)
+
+	outChannels := make([]chan string, len(outputs))
+	for i, name := range outputs {
+		outChannels[i] = c.ensureChannel(name)
+	}
+
+	c.handlers = append(c.handlers, func(ctx context.Context) error {
+		return handlerFunc(ctx, inChannel, outChannels)
+	})
+
+	return nil
+}
+
+func (c *conveyerImpl) RegisterMultiplexer(
+	handlerFunc func(context.Context, []chan string, chan string) error,
+	inputs []string, output string,
+) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.started {
+		return errCannotRegisterAfterRun
+	}
+
+	inChannels := make([]chan string, len(inputs))
+	for i, name := range inputs {
+		inChannels[i] = c.ensureChannel(name)
+	}
+
+	outChannel := c.ensureChannel(output)
+
+	c.handlers = append(c.handlers, func(ctx context.Context) error {
+		return handlerFunc(ctx, inChannels, outChannel)
+	})
+
+	return nil
+}
+
+func (c *conveyerImpl) Run(ctx context.Context) error {
+	c.mu.Lock()
+	if c.started {
+		c.mu.Unlock()
+
+		return errAlreadyStarted
+	}
+
+	c.started = true
+	c.mu.Unlock()
+
+	group, groupCtx := errgroup.WithContext(ctx)
+
+	for _, handler := range c.handlers {
+		h := handler
+
+		group.Go(func() error {
+			return h(groupCtx)
+		})
+	}
+
+	err := group.Wait()
+
+	c.close()
+
+	if err != nil {
+		return fmt.Errorf("errgroup wait: %w", err)
+	}
+
+	return nil
+}
+
+func (c *conveyerImpl) close() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for _, channel := range c.channels {
+		func(ch chan string) {
+			defer func() { _ = recover() }()
+			close(ch)
+		}(channel)
+	}
+}
+
+func (c *conveyerImpl) Send(input, data string) error {
+	c.mu.RLock()
+	channel, exists := c.channels[input]
+	c.mu.RUnlock()
+
+	if !exists {
+		return errChanNotFound
+	}
+
+	channel <- data
+
+	return nil
+}
+
+func (c *conveyerImpl) Recv(output string) (string, error) {
+	c.mu.RLock()
+	channel, exists := c.channels[output]
+	c.mu.RUnlock()
+
+	if !exists {
+		return "", errChanNotFound
+	}
+
+	data, ok := <-channel
+	if !ok {
+		return recvValueOnClosedChannel, nil
+	}
+
+	return data, nil
+}
---
+++ vladislav.peleev/task-5/pkg/handlers/handlers.go
@@ -0,0 +1,106 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+)
+
+var errCantBeDecorated = errors.New("can't be decorated")
+
+const (
+	decoratedPrefix   = "decorated: "
+	noDecoratorSubstr = "no decorator"
+	noMultiplexSubstr = "no multiplexer"
+)
+
+func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(data, noDecoratorSubstr) {
+				return errCantBeDecorated
+			}
+
+			if !strings.HasPrefix(data, decoratedPrefix) {
+				data = decoratedPrefix + data
+			}
+
+			select {
+			case output <- data:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	counter := 0
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			idx := counter % len(outputs)
+			select {
+			case outputs[idx] <- data:
+			case <-ctx.Done():
+				return nil
+			}
+
+			counter++
+		}
+	}
+}
+
+func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	var waitGroup sync.WaitGroup
+
+	for _, ch := range inputs {
+		inCh := ch
+
+		waitGroup.Add(1)
+
+		go func(inputChan chan string) {
+			defer waitGroup.Done()
+
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case data, ok := <-inputChan:
+					if !ok {
+						return
+					}
+
+					if strings.Contains(data, noMultiplexSubstr) {
+						continue
+					}
+
+					select {
+					case output <- data:
+					case <-ctx.Done():
+						return
+					}
+				}
+			}
+		}(inCh)
+	}
+
+	waitGroup.Wait()
+
+	return nil
+}
